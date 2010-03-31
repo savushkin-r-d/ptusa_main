69c6d4afd32fa7167b43eee66279078a13781f54
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2010-03-31 06:30:33 +0000

    mod. Модификация файлов для ucLinux.

--- PAC/common/PAC_dev.h
+++ PAC/common/PAC_dev.h
@@ -40,25 +40,162 @@ template < class data_type > class array_device: public i_simple_device
         data_type*      prev_val;       //ћассив предыдущих значений.
 
     public:        
-        array_device( u_int_4 n, char *new_name, u_int_2 new_subdev_cnt,
-            char type );
+        array_device( u_int_4 n, 
+            char *new_name,
+            u_int_2 new_subdev_cnt,
+            char type ): sub_dev_cnt( new_subdev_cnt ), 
+            type( type ), 
+            n( n )
+            {
+            name = new char[ strlen( new_name ) + 1 ];
+            strcpy( name, new_name );
 
-        int  save_device( char *buff );
-        int  save_state( char *buff );
-        int  save_changed_state( char *buff );
+            prev_val = new data_type[ sub_dev_cnt ];
+            for ( int i = 0; i < sub_dev_cnt; i++ )
+                {
+                prev_val[ i ] = 0;
+                }              
+            }
+
+        int  save_device( char *buff )
+            {
+            // ƒанные группы (buff) в следующем виде:
+            //    1 байт  - тип;                                  (1)
+            //    4 байта - номер;                                (2)
+            //    1 байт  - длина имени группы устройства;        (3)
+            //    х байт  - им€ группы устройства;                (4)
+            //    4 байта - количество подустройств;              (5)
+
+            u_int_2 idx = 0;
+
+            buff[ idx++ ] = type;                             //(1)                            
+            ( ( u_int_4* ) ( buff + idx ) )[ 0 ] = n;         //(2)
+            idx += 4;
+            buff[ idx++ ] = strlen( name );                   //(3)              
+            strcpy( buff + idx, name );                       //(4)
+            idx += strlen( name ) + 1;
+            ( ( u_int_4* ) ( buff + idx ) )[ 0 ] = sub_dev_cnt;//(5)
+            idx += 4;           
+
+            return idx;
+
+            }
+
+        int  save_state( char *buff )
+            {
+            // ƒанные группы (buff) в следующем виде:
+            //  4 байта - номер устройства;                       (1)
+            //  4 байта - количество подустройств;                (2)
+            //  далее   - данные каждого подустройства.
+            ( ( u_int_4* ) buff )[ 0 ] = n;                   //(1)
+            ( ( u_int_4* ) buff )[ 1 ] = sub_dev_cnt;	      //(2)
+            u_int_2 answer_size = 8;
+
+            for ( u_int_4 i = 0; i < sub_dev_cnt; i++ )
+                {
+                //( ( data_type* ) ( buff + 8 + i*sizeof( data_type ) ) )[ 0 ] = 
+                //    get_val( i );
+
+                ( ( data_type* ) ( buff + 8 ) )[ 0 ] = 
+                    get_val( i );
+                buff += sizeof( data_type );
+                prev_val[ i ] = get_val( i );
+                answer_size += sizeof( data_type );      
+                }      
+
+            return answer_size;
+
+            }
+        int  save_changed_state( char *buff )
+            {
+            // ƒанные группы (buff) в следующем виде:          
+            //  2 байта - количество устройств, измененных свое состоние; (1)
+            //  2 байта - номер устройства в массиве устройств.           (2)
+            //  далее   - его измененое состо€ние                         (3)
+            // —охран€ем переданное значение как предыдущее значение.     (4)
+            //»змен€ем размер ответа.                                     (5)
+            //”величиваем на 1 количество устройств, изменивщих свое
+            //состо€ние.                                                  (6)
+            //≈сли нет устройств, изменивших свое состо€ние, возвращаем 0.(7)
+            u_int_2 *changed_dev_cnt = ( u_int_2* ) ( buff );	      //1
+            *changed_dev_cnt = 0;
+            u_int_2 answer_size = 2;
+
+            for ( u_int_2 i = 0; i < sub_dev_cnt; i++ )
+                {
+                if ( get_val( i ) != prev_val[ i ] )
+                    {
+                    ( ( u_int_2* ) ( buff + answer_size ) )[ 0 ] = i; //2
+                    answer_size += 2;
+                    ( ( data_type* ) ( buff + answer_size ) )[ 0 ] =  //3
+                        get_val( i );
+                    prev_val[ i ] = get_val( i );                     //4
+                    answer_size += sizeof( data_type );               //5
+                    ( *changed_dev_cnt )++;                           //6
+                    }
+                }
+            if ( 2 == answer_size )                                   //7
+                {
+                return 0;
+                }
+
+            return answer_size;              
+
+            }
 
         virtual data_type get_val( int idx ) = 0;
 
-        void    print() const;
-        u_int_4 get_n() const;
+        void    print() const
+            {
+            char tmp_str[ 100 ];    
+            //sprintf( tmp_str, "\"%s\", \t[ %3lu ]", name, sub_dev_cnt );
+            if ( strlen( name ) < 8 ) 
+                {
+                sprintf( tmp_str, "\"%s\", \t\t[ %3lu ]", name, sub_dev_cnt );
+                }
+            else sprintf( tmp_str, "\"%s\", \t[ %3lu ]", name, sub_dev_cnt );
 
-        int load_state( char *buff );
-        int load_changed_state( char *buff );
-        int load_device( char *buff );
+            print_str( tmp_str );
+            }
 
-        int     parse_cmd( char *buff  );
-        u_int_4 get_idx();
-        void    set_idx( u_int_4 new_idx );
+        u_int_4 get_n() const
+            { 
+            return 0; 
+            } 
+
+        int load_state( char *buff )
+            { 
+            buff;
+            return 0; 
+            } 
+
+        int load_changed_state( char *buff )
+            { 
+            buff;
+            return 0; 
+            } 
+
+        int load_device( char *buff )
+            { 
+            buff;
+            return 0; 
+            } 
+
+        int     parse_cmd( char *buff  )
+            {      
+            buff;
+            return 0; 
+            } 
+
+        u_int_4 get_idx()
+            {
+            return 0;
+            }
+
+        void set_idx( u_int_4 new_idx )
+            {  
+            new_idx;
+            }
     };
 //-----------------------------------------------------------------------------
 #endif // W750
