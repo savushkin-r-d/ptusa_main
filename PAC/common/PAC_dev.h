c3bb62242fa804d8826b69c9ba2294e488464496
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2010-04-16 05:50:48 +0000

    new. Работа над устройствами Wago.

--- PAC/common/PAC_dev.h
+++ PAC/common/PAC_dev.h
@@ -12,7 +12,7 @@
 /// @$Rev$.\n
 /// @$Author$.\n
 /// @$Date::                     $.
- 
+
 #ifndef PAC_DEVICES_H
 #define PAC_DEVICES_H
 
@@ -20,12 +20,13 @@
 #include <stdio.h>
 
 #include "g_device.h"
+#include "wago.h"
 //-----------------------------------------------------------------------------
 /// @brief Шаблон класса, который используется для передачи состояния устройств,
 /// которые представляются массивом некоторого типа.
 //
 /// Например: параметры, состояния и т.д.
-template < class data_type > class array_device: public i_simple_device
+template < class data_type > class i_array_device: public i_simple_device
     {
     public:
         /// @param n        - номер.
@@ -33,7 +34,7 @@ template < class data_type > class array_device: public i_simple_device
         /// @param new_subdev_cnt - количество элементов в массиве.
         /// @param type     - тип. Для сохранения устройств и передачи на
         /// сервер.
-        array_device( u_int_4 n, 
+        i_array_device( u_int_4 n, 
             const char *new_name,
             u_int_2 new_subdev_cnt,
             char type ): sub_dev_cnt( new_subdev_cnt ), 
@@ -122,12 +123,12 @@ template < class data_type > class array_device: public i_simple_device
         int  save_changed_state( char *buff )
             {
             // Данные группы (buff) в следующем виде:          
-            //  2 байта - количество устройств, измененных свое состоние;   (1)
+            //  2 байта - количество устройств, изменивших свое состояние;  (1)
             //  2 байта - номер устройства в массиве устройств.             (2)
-            //  далее   - его измененое состояние                           (3)
+            //  далее   - его измененное состояние                          (3)
             // Сохраняем переданное значение как предыдущее значение.       (4)
             //Изменяем размер ответа.                                       (5)
-            //Увеличиваем на 1 количество устройств, изменивщих свое
+            //Увеличиваем на 1 количество устройств, изменивших свое
             //состояние.                                                    (6)
             //Если нет устройств, изменивших свое состояние, возвращаем 0.  (7)
             u_int_2 *changed_dev_cnt = ( u_int_2* ) ( buff );               //1
@@ -153,10 +154,12 @@ template < class data_type > class array_device: public i_simple_device
                 }
 
             return answer_size;              
-
             }
 
-        /// @brief Получение элемента через индекс.
+        /// @brief Получение значения элемента через индекс. 
+        ///
+        /// Это значение потом, например, может используется для сохранения 
+        /// объекта в поток ( @ref save_state ).
         ///
         /// @param idx - индекс элемента.
         ///
@@ -176,7 +179,7 @@ template < class data_type > class array_device: public i_simple_device
                     name, ( unsigned long int ) sub_dev_cnt );
                 }
             else sprintf( tmp_str, "\"%s\", \t[ %3lu ]", 
-                    name, ( unsigned long int ) sub_dev_cnt );
+                name, ( unsigned long int ) sub_dev_cnt );
 
             print_str( tmp_str );
             }
@@ -187,46 +190,6 @@ template < class data_type > class array_device: public i_simple_device
             return 0; 
             } 
 
-        /// @brief Реализация-заглушка интерфейса класса @ref i_simple_device.
-        int load_state( char *buff )
-            { 
-            buff++; // Чтобы не было warning'а.
-            return 0; 
-            } 
-
-        /// @brief Реализация-заглушка интерфейса класса @ref i_simple_device.
-        int load_changed_state( char *buff )
-            { 
-            buff++; // Чтобы не было warning'а.
-            return 0; 
-            } 
-
-        /// @brief Реализация-заглушка интерфейса класса @ref i_simple_device.
-        int load_device( char *buff )
-            { 
-            buff++; // Чтобы не было warning'а.
-            return 0; 
-            } 
-
-        /// @brief Реализация-заглушка интерфейса класса @ref i_simple_device.
-        int parse_cmd( char *buff  )
-            {      
-            buff++; // Чтобы не было warning'а.
-            return 0; 
-            } 
-
-        /// @brief Реализация-заглушка интерфейса класса @ref i_simple_device.
-        u_int_4 get_idx()
-            {
-            return 0;
-            }
-
-        /// @brief Реализация-заглушка интерфейса класса @ref i_simple_device.
-        void set_idx( u_int_4 new_idx )
-            {  
-            new_idx++; // Чтобы не было warning'а.
-            }
-
     protected:
         u_int_4         sub_dev_cnt;    ///< Количество подустройств.
         char            *name;          ///< Имя.
@@ -234,6 +197,333 @@ template < class data_type > class array_device: public i_simple_device
         u_int_4         n;              ///< Уникальный номер.
 
         data_type*      prev_val;       ///< Массив предыдущих значений.
+    };
+//-----------------------------------------------------------------------------
+/// @brief 
+//
+///
+class device : public i_simple_device
+    {
+    public:
+        enum DEVICE_TYPE
+            {
+            DT_V = 0, 
+            DT_N,
+            DT_M,
+            DT_LS,     
+            DT_TE,     
+            DT_FE,     
+            DT_FS,     
+            DT_CTR,    
+            DT_AO,     
+            DT_LE,
+            DT_FB,     
+            DT_UPR,    
+            DT_QE,    
+            DT_AI,
+            };
+
+        enum DEVICE_SUB_TYPE
+            {
+            //DT_V = 0, 
+            DST_V_1DO = 0,            
+            DST_V_2DO,
+            DST_V_1DO_1DI,
+            DST_V_1DO_2DI,
+            DST_V_2DO_2DI,
+            DST_V_1DO_3DI, 
+            DST_V_1DO_2DI_S,    // 2 FB на одно из состояний.
+            DST_V_AS_MIX,       // Микспруф с AS интерфейсом.
+            };
+
+        virtual void on() = 0;
+        virtual void off() = 0;
+        virtual u_int_4 get_state() = 0;
+        virtual int     set_state( u_int_4 new_state ) = 0;
+
+        virtual float get_value() = 0;
+        virtual int   set_value( float new_value ) = 0;
+
+        // Сохранение самого устройства в буфер.
+        int save_device( char *buff  )
+            {
+            ( ( u_int_4* ) buff )[ 0 ] = number;
+            return sizeof( u_int_4 );
+            }
+
+        void print() const
+            {  
+#ifdef DEBUG
+            Print( "%5lu\n", ( unsigned long ) number );
+#endif // DEBUG            
+            }
+
+        virtual int load( char *stream )
+            {
+            number = *( ( u_int_4* ) stream );
+            return sizeof( u_int_4 );
+            }
+
+        //Возвращает номер устройства.
+        u_int_4 get_n() const
+            {
+            return number;
+            }
+
+    protected:
+        u_int_4 number;
+
+        DEVICE_TYPE     type;
+        DEVICE_SUB_TYPE sub_type;
+    };
+//-----------------------------------------------------------------------------
+/// @brief 
+//
+/// 
+class DI_device : public i_save_device
+    {
+    public:
+        DI_device(): prev_state( 0 )
+            {            
+            }
+
+        virtual int get_state() = 0;
+
+        // Сохранение измененного состояния устройства в буфер.
+        int save_changed_state( char *buff )
+            {
+            if ( prev_state != get_state() )
+                {
+                buff[ 0 ] = get_state();				
+                prev_state = get_state();
+                return sizeof( char );
+                }                      
+            return 0;
+            }
+
+        // Сохранение состояния устройства в буфер.
+        int save_state( char *buff  )
+            {
+            buff[ 0 ] = get_state();
+            prev_state = get_state();
+            return sizeof( char );
+            }
+
+   private:
+        char prev_state;
+    };
+//-----------------------------------------------------------------------------
+/// @brief 
+//
+/// 
+class DO_device: public DI_device
+    {
+    public:
+        virtual void on() = 0;
+        virtual void off() = 0;
+
+        virtual int set_state( int new_value ) = 0;   
+    };
+//-----------------------------------------------------------------------------
+/// @brief 
+//
+/// 
+class AI_device
+    {
+    public:
+        virtual float get_value() = 0;
+    };
+//-----------------------------------------------------------------------------
+/// @brief 
+//
+/// 
+class AO_device: public AI_device
+    {
+    public:
+        virtual void off() = 0;
+
+        virtual float set_value( float new_value ) = 0;
+    };
+//-----------------------------------------------------------------------------
+/// @brief 
+//
+/// 
+class wago_state_device
+    {
+    public:
+        wago_state_device(): DI_count( 0 ), DI_tables( 0 ), DI_offsets( 0 ),
+            DO_count( 0 ), DO_tables( 0 ), DO_offsets( 0 ),
+            AI_count( 0 ), AI_tables( 0 ), AI_offsets( 0 ),
+            AO_count( 0 ), AO_tables( 0 ), AO_offsets( 0 )
+            {
+            }            
+
+        virtual int load( char *stream )
+            {
+            //[ 0   ] - номер устройства.
+            //[ 1   ] - 
+            //[ 2   ] - 
+            //[ 3   ] - 
+            int pos = device::load( stream );
+
+            //[ 4   ] - количество DI.
+            //[ 5   ] - 
+            //[ 6   ] - номер таблицы DI.
+            //[ 7   ] - 
+            //[ 8   ] - смещение в пределах таблицы.
+            //[ ... ] -   
+            //[ x1  ] - количество DO.
+            //[ ... ] -   
+            //[ x2  ] - количество AO.
+            //[ ... ] -   
+            //[ x3  ] - количество AI.
+            //[ ... ] -            
+            pos += load_table( stream + pos, DI_count, DI_tables, DI_offsets );
+            pos += load_table( stream + pos, DO_count, DO_tables, DO_offsets );
+            pos += load_table( stream + pos, AI_count, AI_tables, AI_offsets );
+            pos += load_table( stream + pos, AO_count, AO_tables, AO_offsets );
+            return pos;
+            }
+
+    protected:
+        int get_DO( u_int index )
+            {
+            return wago_manager::get_instance()->get_DO( DO_tables[ index ],
+                DO_offsets[ index ] );
+            }
+        int set_DO( u_int index, char value )
+            {
+            return wago_manager::get_instance()->set_DO( DO_tables[ index ],
+                DO_offsets[ index ], value );
+            }
+
+        int get_DI( u_int index )
+            {
+            return wago_manager::get_instance()->get_DI( DO_tables[ index ],
+                DO_offsets[ index ] );
+            }
+        int set_DI( u_int index, char value )
+            {
+            return wago_manager::get_instance()->set_DI( DO_tables[ index ],
+                DO_offsets[ index ], value );
+            }
+
+        float get_AO( u_int index )
+            {
+            return wago_manager::get_instance()->get_AO( DO_tables[ index ],
+                DO_offsets[ index ] );
+            }
+        int   set_AO( u_int index, float value )
+            {
+            return wago_manager::get_instance()->set_AO( DO_tables[ index ],
+                DO_offsets[ index ], value );
+            }
+
+        float get_AI( u_int index )
+            {
+            return wago_manager::get_instance()->get_AI( DO_tables[ index ],
+                DO_offsets[ index ] );
+            }
+        int   set_AI( u_int index, float value )
+            {
+            return wago_manager::get_instance()->set_AI( DO_tables[ index ],
+                DO_offsets[ index ], value );
+            }
+
+    private:
+        u_int_2 DI_count;
+        int_2*  DI_tables;
+        int_2*  DI_offsets;
+
+        u_int_2 DO_count;
+        int_2*  DO_tables;
+        int_2*  DO_offsets;
+
+        u_int_2 AI_count;
+        int_2*  AI_tables;
+        int_2*  AI_offsets;
+
+        u_int_2 AO_count;
+        int_2*  AO_tables;
+        int_2*  AO_offsets;
+
+        int load_table( char *stream, u_int_2 &count, u_int_2 *tables,
+            u_int_2 *offsets )
+            {
+            //[ 0   ] - количество DI (например).
+            //[ 1   ] - 
+            //[ 2   ] - номер таблицы DI №1.
+            //[ 3   ] - 
+            //[ 4   ] - смещение в пределах таблицы №1.
+            //[ 5   ] -
+            //[ 6   ] - номер таблицы DI №2.
+            //[ 7   ] - 
+            //[ 8   ] - смещение в пределах таблицы №2.
+            //[ ... ] -  
+            count = *( ( u_int_2 * ) stream );
+            int pos = sizeof( u_int_2 );
+
+            if ( count > 0 )
+                {
+                tables = new u_int_2[ count ];
+                offsets = new u_int_2[ count ];
+                for ( u_int i = 0; i < count; i++ )
+                    {
+                    tables[ i ] = *( ( u_int_2 * ) ( stream + pos ) );
+                    pos += sizeof( u_int_2 );
+
+                    offsets[ i ] = *( ( u_int_2 * ) ( stream + pos ) );
+                    pos += sizeof( u_int_2 );
+                    }
+                }
+
+            return pos;
+            }
+    };
+//-----------------------------------------------------------------------------
+/// @brief 
+//
+/// 
+class DO_1 : public DO_device, wago_state_device, device
+    {
+    public:    
+        int get_state()
+            {
+            return get_DO( DO_INDEX );
+            }
+
+        void on() 
+            {
+            return set_DO( DO_INDEX, 1 );
+            }
+
+        void off()
+            {
+            return set_DO( DO_INDEX, 0 );
+            }
+
+        int set_state( int new_value )
+            {
+            return set_DO( DO_INDEX, new_value );
+            }
+
+    private:
+        enum CONSTANTS
+            {
+            DO_INDEX = 0,
+            };
+    };
+//-----------------------------------------------------------------------------
+/// @brief 
+//
+/// 
+class device_manager
+    {
+    public:
+        int load_from_stream( char *stream );
+
+    private:
+        device **project_devices;
     };
 //-----------------------------------------------------------------------------
 #endif // PAC_DEVICES_H
