8f182dd9aa1c97cea0de9b4bd7829e431461e65a
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2010-09-30 08:49:43 +0000

    mod. Поддержка Visual Studio.

--- PAC_control_projects/whey_out/main.cpp
+++ PAC_control_projects/whey_out/main.cpp
@@ -12,8 +12,21 @@
 
 #include "sys.h"
 #include "PAC_dev.h"
+
+#ifdef LINUX_OS
 #include "tcp_cmctr_linux.h"
 #include "prj_mngr_linux.h"
+#endif // LINUX_OS
+
+#ifdef WIN_OS
+#include "StdAfx.h"
+
+#include <conio.h>
+
+#include "tcp_cmctr_win.h"
+#include "prj_mngr_win.h"
+#endif // WIN_OS
+
 
 #ifdef PAC_WAGO_750_860
 #include "sys_w750.h"
@@ -45,9 +58,18 @@ int main( int argc, char *argv[] )
     fprintf( stderr, "Program started - %s\n", asctime( localtime( &t ) ) );
 #endif // defined LINUX_OS
 
+#ifdef LINUX_OS
     project_manager::set_instance( new project_manager_linux() );
     tcp_communicator::set_instance( 
         new tcp_communicator_linux( "Выдача сыворотки" ) );
+#endif // LINUX_OS
+
+#ifdef WIN_OS
+    project_manager::set_instance( new project_manager_win() );
+    tcp_communicator::set_instance( 
+        new tcp_communicator_win( "Выдача сыворотки" ) );
+#endif // WIN_OS
+
     device_manager::set_instance( new device_manager() );
     device_communicator::set_instance( new device_communicator() );
 #if defined LINUX_OS
@@ -61,11 +83,26 @@ int main( int argc, char *argv[] )
 #endif // PAC_PC
 #endif // defined LINUX_OS
 
+#if defined WIN_OS
+    wago_manager::set_instance( new wago_manager_PC() );
+    NV_memory_manager::set_instance( new NV_memory_manager_PC() );
+#endif // defined WIN_OS
+
     tech_object_manager::set_instance( new tech_object_manager() );
     PAC_critical_errors_manager::set_instance( new PAC_critical_errors_manager() );
 
     G_PROJECT_MANAGER->proc_main_params( argc, argv );
+#ifdef LINUX_OS
     G_PROJECT_MANAGER->load_configuration( "./whey_out.ds5" );
+#endif // LINUX_OS
+
+#ifdef WIN_OS
+#ifdef DEBUG
+     G_PROJECT_MANAGER->load_configuration( "..\\whey_out.ds5" );
+#else
+    G_PROJECT_MANAGER->load_configuration( "whey_out.ds5" );
+#endif // DEBUG
+#endif // WIN_OS
 
 #ifdef DEBUG
     G_DEVICE_MANAGER()->print();
@@ -73,9 +110,9 @@ int main( int argc, char *argv[] )
 
     G_CMMCTR->reg_service( device_communicator::C_SERVICE_N,
         device_communicator::write_devices_states_service );
-    
+
     lua_manager::get_instance()->init();        //-Инициализация Lua.
-    
+
     G_TECH_OBJECT_MNGR()->init_objects();    
     for ( u_int i = 0; i < G_TECH_OBJECT_MNGR()->get_count(); i++ )
         {
@@ -83,13 +120,13 @@ int main( int argc, char *argv[] )
 
         G_DEVICE_CMMCTR->add_device( G_TECH_OBJECTS( i )->get_complex_dev() );        
         }
-    
+
 #ifdef DEBUG
     G_DEVICE_CMMCTR->print();
 
-    ulong st_time;
-    ulong all_time = 0;
-    ulong cycles_cnt = 0;
+    u_long st_time;
+    u_long all_time = 0;
+    u_long cycles_cnt = 0;
 
     while ( !kbhit() )
 #else
@@ -102,7 +139,7 @@ int main( int argc, char *argv[] )
 #endif // DEBUG
 
 #ifndef DEBUG_NO_WAGO_MODULES
-       G_WAGO_MANAGER->read_inputs();
+        G_WAGO_MANAGER->read_inputs();
 #endif // DEBUG_NO_WAGO_MODULES
 
         G_TECH_OBJECT_MNGR()->evaluate();
@@ -110,7 +147,7 @@ int main( int argc, char *argv[] )
         G_CMMCTR->evaluate();
 
 #ifndef DEBUG_NO_WAGO_MODULES
-       G_WAGO_MANAGER->write_outputs();
+        G_WAGO_MANAGER->write_outputs();
 #endif // ifndef
 
         PAC_critical_errors_manager::get_instance()->show_errors();
@@ -120,15 +157,19 @@ int main( int argc, char *argv[] )
 
 #if defined LINUX_OS
 #ifdef PAC_PC
-       const u_int MAX_ITERATION = 100000;
+        const u_int MAX_ITERATION = 100000;
 #endif // PAC_PC
 #ifdef PAC_WAGO_750_860
-       const u_int MAX_ITERATION = 1000;
+        const u_int MAX_ITERATION = 1000;
 #endif // PAC_WAGO_750_860
 #endif // defined LINUX_OS
 
-       static char print_cycle_time_count = 0;
-       if ( print_cycle_time_count < 10 && cycles_cnt > MAX_ITERATION )
+#ifdef WIN_OS
+        const u_int MAX_ITERATION = 1000;
+#endif // WIN_OS
+
+        static char print_cycle_time_count = 0;
+        if ( print_cycle_time_count < 10 && cycles_cnt > MAX_ITERATION )
             {
             print_time( "\tMain cycle avg time = %lu\n", all_time / cycles_cnt  );
             all_time = 0;
@@ -137,7 +178,7 @@ int main( int argc, char *argv[] )
             print_cycle_time_count++;
             }
 #endif // DEBUG
-       }   
+        }   
 
     return( EXIT_SUCCESS );
     }
