1d0e7ad7f7a7477e4d004db5a436fdf61c55b669
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2010-10-19 06:38:14 +0000

    mod. При ошибках загрузки описания устройств - аварийное завершение программы.

--- PAC/common/prj_mngr.cpp
+++ PAC/common/prj_mngr.cpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 
 #include "prj_mngr.h"
 #include "wago.h"
@@ -9,20 +10,6 @@ auto_smart_ptr < project_manager > project_manager::instance;
 //-----------------------------------------------------------------------------
 int project_manager::proc_main_params( int argc, char *argv[] )
     {
-//      if ( argc > 1 )
-//        {
-//        if ( strcmp( argv[ 1 ], "rcrc" ) == 0 )
-//            {
-//#ifdef DEBUG
-//            Print( "\nReset CRC..." );
-//#endif
-//            params_manager::get_instance()->reset_CRC();
-//#ifdef DEBUG
-//            Print( " Ok!\n" );
-//#endif
-//            }
-//
-//        }
     for ( int i = 1; i < argc; i++ )
         {
         if ( strcmp( argv[ i ], "rcrc" ) == 0 )
@@ -49,13 +36,14 @@ int project_manager::load_configuration( const char *file_name )
     if ( cfg_file->file_open( file_name ) <= 0 )
         {
 #ifdef DEBUG
-        Print( "project_manager:load_configuration(...) - file not found!\n" );            
+        Print( "project_manager:load_configuration(...) - file not found!\n" );
 #endif // DEBUG
-        while ( 1 ) { }
+
+        exit( 1 );
         }
 
     const char *SIGNATURE            = "WG {4B714C08-9602-4130-8563-4B51E08BB9D7}";
-    const int   SIGNATURE_SIZE       = 42;    
+    const int   SIGNATURE_SIZE       = 42;
     char        id[ SIGNATURE_SIZE ] = { 0 };
 
     cfg_file->file_read( id, SIGNATURE_SIZE - 1 );
@@ -64,9 +52,9 @@ int project_manager::load_configuration( const char *file_name )
     if ( strcmp( id, SIGNATURE ) != 0 )
         {
 #ifdef DEBUG
-        Print( "project_manager:load_configuration(...) - not correct file format!\n" );            
+        Print( "project_manager:load_configuration(...) - not correct file format!\n" );
 #endif // DEBUG
-        while ( 1 ) { }
+        exit( 1 );
         }
 
     //-Editor editor_version.
@@ -80,7 +68,7 @@ int project_manager::load_configuration( const char *file_name )
         Print( "project_manager:load_configuration(...) - not correct editor editor_version - %d, must be %d!\n",
             editor_version, CURRENT_EDITOR_VERSION );
 #endif // DEBUG
-        while ( 1 ) { }
+        exit( 1 );
         }
 
     //-File editor_version.
@@ -96,14 +84,14 @@ int project_manager::load_configuration( const char *file_name )
 
     //-Wago data.
     wago_manager::get_instance()->load_from_cfg_file( cfg_file );
-    
+
     //-Devices data.
     device_manager::get_instance()->load_from_cfg_file( cfg_file );
 
     cfg_file->file_close();
 
     G_DEVICE_CMMCTR->add_device( device_manager::get_instance()->get_device() );
-  
+
 #ifdef DEBUG
     Print( "Reading completed.\n\n", file_name );
 #endif // DEBUG
