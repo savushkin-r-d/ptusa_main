35f5967354bdaf6af5b053d3e0305b08e2ebf493
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2015-04-30 06:08:02 +0000

    fix. Корректная работа с системным временем.

--- PAC/linux/l_console.h
+++ PAC/linux/l_console.h
@@ -26,16 +26,10 @@
 //-----------------------------------------------------------------------------
 extern time_t t_;
 extern struct tm *timeInfo_;
-#ifdef PAC_WAGO_PFC200
-#define print_time t_ = time( 0 ); timeInfo_ = gmtime( &t_ );\
-     printf( "%02d.%02d.%02d %02d:%02d:%02d ",\
-     timeInfo_->tm_mday, timeInfo_->tm_mon + 1, timeInfo_->tm_year,\
-     timeInfo_->tm_hour, timeInfo_->tm_min, timeInfo_->tm_sec ); printf
-#else
+
 #define print_time t_ = time( 0 ); timeInfo_ = localtime( &t_ );\
      printf( "%02d.%02d.%02d %02d:%02d:%02d ",\
      timeInfo_->tm_mday, timeInfo_->tm_mon + 1, timeInfo_->tm_year,\
      timeInfo_->tm_hour, timeInfo_->tm_min, timeInfo_->tm_sec ); printf
-#endif
 //-----------------------------------------------------------------------------
 #endif // L_CONSOLE_H
