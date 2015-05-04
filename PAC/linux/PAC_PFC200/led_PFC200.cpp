78df05b8f6e63127271fc13e01534a49c78a0df3
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2015-05-04 08:13:20 +0000

    mod. Теперь для работы с LED-индикаторами используются системные константы.

--- PAC/linux/PAC_PFC200/led_PFC200.cpp
+++ PAC/linux/PAC_PFC200/led_PFC200.cpp
@@ -1,6 +1,7 @@
 #include "led_PFC200.h"
 extern "C" {
-    #include <diagnostic/diagnostic_API.h>
+#include <diagnostic/diagnostic_API.h>
+#include "diagnostic/led_server.h"
     }
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
@@ -14,7 +15,7 @@ led_PFC200::~led_PFC200( )
     }
 //-----------------------------------------------------------------------------
 void led_PFC200::on( LEDS_NAMES led, COLOUR led_color )
-    {    
+    {
     switch ( led )
         {
         case L_ALL:
@@ -24,15 +25,15 @@ void led_PFC200::on( LEDS_NAMES led, COLOUR led_color )
             switch ( led_color )
                 {
                 case C_GREEN:
-                    log_EVENT_LogId( 0x00050010, true );
+                    log_EVENT_LogId( DIAG_LED_USER1_STATIC_GRN, true );
                     break;
 
                 case C_RED:
-                    log_EVENT_LogId( 0x00050011, true );
+                    log_EVENT_LogId( DIAG_LED_USER1_STATIC_RED, true );
                     break;
-                    
+
                 case C_YELLOW:
-                    log_EVENT_LogId( 0x00050012, true );
+                    log_EVENT_LogId( DIAG_LED_USER1_STATIC_YLW, true );
                     break;
                 }
             break;
@@ -46,15 +47,15 @@ void led_PFC200::on( LEDS_NAMES led, COLOUR led_color )
     }
 //-----------------------------------------------------------------------------
 void led_PFC200::off( LEDS_NAMES led )
-    {    
+    {
     switch ( led )
         {
         case L_ALL:
-            log_EVENT_LogId( 0x00050000, true ); //Switch off U1-LED.
+            log_EVENT_LogId( DIAG_LED_USER1_STATIC_OFF, true );
             break;
 
         case L_STATUS:
-            log_EVENT_LogId( 0x00050000, true ); //Switch off U1-LED.
+            log_EVENT_LogId( DIAG_LED_USER1_STATIC_OFF, true );
             break;
 
         case L_SERVICE:
