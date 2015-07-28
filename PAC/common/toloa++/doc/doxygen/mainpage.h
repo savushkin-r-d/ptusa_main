58228f14e39a374043dee9ed3cd49f6d714d4d03
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2015-07-28 07:35:32 +0000

    new. Поддержка двойных комментариев (на русском и английском языках).

--- PAC/common/toloa++/doc/doxygen/mainpage.h
+++ PAC/common/toloa++/doc/doxygen/mainpage.h
@@ -1,12 +1,21 @@
 ///@mainpage
 ///
-///@section main Общие сведения.
+///@section main Brief description
+///@if rus_dox
 /// Управляющая логика реализуется в виде скрипта Lua. В ходе выполнения программы
 /// управление передается в отдельные специальные функции Lua, которые отвечают 
 /// за отдельные этапы.
-/// 
-///@section intro Описание взаимодействия скриптов Lua и управляющей программы.
-/// Общая схема работы управляющей программы приведена на рисунке ниже.<br>
+///@endif
+///
+///@if eng_dox
+/// Control logic is written as Lua script. During program execution special Lua 
+/// functions realize user logic.
+///@endif
+///
+///
+///@section intro Lua and main program
+///@if rus_dox
+/// Общую схему работы управляющей программы смотри на рис. 1.<br>
 /// @image html main.jpg "Рис.1 Работа управляющей программы"
 /// 
 /// Управляющий цикл выглядит так (рис. 2):<br>
@@ -17,4 +26,5 @@
 /// 
 /// При выполнении команды происходит следующее (рис. 4):<br>
 /// @image html tech_object__exec_cmd.jpg "Рис.4 Выполнение команды"
+///@endif
 
