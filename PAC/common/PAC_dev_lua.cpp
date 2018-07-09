4dc3861a9f8e7a63a5d80c09478c4d6029e41766
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2018-07-09 06:15:13 +0000

    new. Добавлена функция для чтения двойного слова (int4).

--- PAC/common/PAC_dev_lua.cpp
+++ PAC/common/PAC_dev_lua.cpp
@@ -1,6 +1,6 @@
 /*
 ** Lua binding: PAC_dev
-** Generated automatically by tolua++-1.0.92 on 05/28/18 10:57:43.
+** Generated automatically by tolua++-1.0.92 on 07/09/18 09:14:06.
 */
 
 #ifndef __cplusplus
@@ -11636,6 +11636,40 @@ static int tolua_PAC_dev_profibus_slave_set_int00(lua_State* tolua_S)
 }
 #endif //#ifndef TOLUA_DISABLE
 
+/* method: get_int4 of class  profibus_slave */
+#ifndef TOLUA_DISABLE_tolua_PAC_dev_profibus_slave_get_int400
+static int tolua_PAC_dev_profibus_slave_get_int400(lua_State* tolua_S)
+{
+#ifndef TOLUA_RELEASE
+ tolua_Error tolua_err;
+ if (
+     !tolua_isusertype(tolua_S,1,"profibus_slave",0,&tolua_err) ||
+     !tolua_isnumber(tolua_S,2,0,&tolua_err) ||
+     !tolua_isnoobj(tolua_S,3,&tolua_err)
+ )
+  goto tolua_lerror;
+ else
+#endif
+ {
+  profibus_slave* self = (profibus_slave*)  tolua_tousertype(tolua_S,1,0);
+  int byte_offset = ((int)  tolua_tonumber(tolua_S,2,0));
+#ifndef TOLUA_RELEASE
+  if (!self) tolua_error(tolua_S,"invalid 'self' in function 'get_int4'", NULL);
+#endif
+  {
+   int tolua_ret = (int)  self->get_int4(byte_offset);
+   tolua_pushnumber(tolua_S,(lua_Number)tolua_ret);
+  }
+ }
+ return 1;
+#ifndef TOLUA_RELEASE
+ tolua_lerror:
+ tolua_error(tolua_S,"#ferror in function 'get_int4'.",&tolua_err);
+ return 0;
+#endif
+}
+#endif //#ifndef TOLUA_DISABLE
+
 /* function: G_PROFIBUS_SLAVE_LUA */
 #ifndef TOLUA_DISABLE_tolua_PAC_dev_G_PROFIBUS_SLAVE_LUA00
 static int tolua_PAC_dev_G_PROFIBUS_SLAVE_LUA00(lua_State* tolua_S)
@@ -12243,6 +12277,7 @@ TOLUA_API int tolua_PAC_dev_open (lua_State* tolua_S)
    tolua_function(tolua_S,"set_bool",tolua_PAC_dev_profibus_slave_set_bool00);
    tolua_function(tolua_S,"get_int",tolua_PAC_dev_profibus_slave_get_int00);
    tolua_function(tolua_S,"set_int",tolua_PAC_dev_profibus_slave_set_int00);
+   tolua_function(tolua_S,"get_int4",tolua_PAC_dev_profibus_slave_get_int400);
   tolua_endmodule(tolua_S);
   tolua_function(tolua_S,"G_PROFIBUS_SLAVE_LUA",tolua_PAC_dev_G_PROFIBUS_SLAVE_LUA00);
  tolua_endmodule(tolua_S);
