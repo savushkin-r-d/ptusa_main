4686a75e497a3f981a7503d13d0b088d2d246df6
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2021-11-11 16:33:15 +0300

    [cmake] Add CMake presets configuration (#327)
    * Add support build binary for AXF2152 on Linux
    
    * Enhance build binary for AXF2152 on Linux
    
    * Add build binary for AXF2152 on Windows
    
    * Refactor
    
    * Add build library for PLCnextEngineer
    
    * Correct PLCnextEngineer library sources
    
    * Fix CMake description
    
    * Rename PLCnextEngineer library source directory
    
    * Fix endings
    
    * Add "PtusaPLCnextEngineer.pcwlx" generation
    
    * Fix PLCnextEngineer library build
    
    * Fix library name on Linux
    
    * Install only *.dll and *.exe files
    
    * Fix version printing for dll on Windows
    
    * [review] Use constants
    
    * Fix PLCnextEngineer library build

---
+++ PAC/linux/PAC_PLCnext/PtusaPLCnextEngineer/PtusaMainCmpnt.cpp
@@ -0,0 +1,34 @@
+﻿#include "PtusaMainCmpnt.hpp"
+#include "Arp/Plc/Commons/Esm/ProgramComponentBase.hpp"
+
+namespace PtusaPLCnextEngineer
+    {
+    void PtusaMainCmpnt::Initialize()
+        {
+        // never remove next line
+        ProgramComponentBase::Initialize();
+
+        // subscribe events from the event system (Nm) here
+        }
+
+    void PtusaMainCmpnt::LoadConfig()
+        {
+        // load project config here
+        }
+
+    void PtusaMainCmpnt::SetupConfig()
+        {
+        // never remove next line
+        ProgramComponentBase::SetupConfig();
+
+        // setup project config here
+        }
+
+    void PtusaMainCmpnt::ResetConfig()
+        {
+        // never remove next line
+        ProgramComponentBase::ResetConfig();
+
+        // implement this inverse to SetupConfig() and LoadConfig()
+        }
+    } // end of namespace PtusaPLCnextEngineer
---
+++ PAC/linux/PAC_PLCnext/PtusaPLCnextEngineer/PtusaMainPrg.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include "Arp/System/Core/Arp.h"
+#include "Arp/Plc/Commons/Esm/ProgramBase.hpp"
+#include "Arp/System/Commons/Logging.h"
+#include "PtusaMainCmpnt.hpp"
+
+namespace PtusaPLCnextEngineer
+    {
+    using namespace Arp;
+    using namespace Arp::System::Commons::Diagnostics::Logging;
+    using namespace Arp::Plc::Commons::Esm;
+
+    //#program
+    //#component(PtusaPLCnextEngineer::PtusaMainCmpnt)
+    class PtusaMainPrg: public ProgramBase, private Loggable<PtusaMainPrg>
+        {
+    public:
+        // typedefs
+
+    public:
+        // construction/destruction
+        PtusaMainPrg(PtusaPLCnextEngineer::PtusaMainCmpnt& ptusaMainCmpntArg,
+                const String& name);
+        PtusaMainPrg(const PtusaMainPrg& arg) = delete;
+        virtual ~PtusaMainPrg() = default;
+
+    public:
+        // operators
+        PtusaMainPrg& operator=(const PtusaMainPrg& arg) = delete;
+
+    public:
+        // properties
+        static const int MAX_NVRAM_SIZE = 49000;
+
+    public:
+        // operations
+        void Execute() override;
+
+        //#port
+        //#attributes(Output|Retain)
+        uint8 NVRAM[ MAX_NVRAM_SIZE ] = {0};
+
+    private:
+        // fields
+        PtusaPLCnextEngineer::PtusaMainCmpnt& ptusaMainCmpnt;
+        };
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // inline methods of class ProgramBase
+    inline PtusaMainPrg::PtusaMainPrg(
+            PtusaPLCnextEngineer::PtusaMainCmpnt& ptusaMainCmpntArg,
+            const String& name) :
+            ProgramBase(name), ptusaMainCmpnt(ptusaMainCmpntArg)
+        {
+        }
+    } // end of namespace PtusaPLCnextEngineer
