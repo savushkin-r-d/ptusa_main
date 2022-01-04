977bd59d30f5e0c1a726a7d58d7e5be1bbaf5bd3
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2022-01-04 11:37:23 +0300

    Refactor tests

--- test/mode_mngr_tests.cpp
+++ test/mode_mngr_tests.cpp
@@ -14,8 +14,16 @@ TEST( open_seat_action, init )
 	mock_params_manager* par_mock = new mock_params_manager();
 	test_params_manager::replaceEntity( par_mock );
 
+	EXPECT_CALL( *par_mock, init( _ ) );
+	EXPECT_CALL( *par_mock, final_init( _, _, _ ) );
+
 	par_mock->init( 0 );
 	par_mock->final_init( 0, 0, 0 );
+
+	saved_params_u_int_4& par = PAC_info::get_instance()->par;
+	par[ PAC_info::P_MIX_FLIP_PERIOD ] = 1;
+	par[ PAC_info::P_MIX_FLIP_UPPER_TIME ] = 100;
+	par[ PAC_info::P_MIX_FLIP_LOWER_TIME ] = 100;
 
 	tech_object test_tank( "Танк1", 1, 1, "T", 10, 10, 10, 10, 10, 10 );
 	DO1 test_DO( "test_DO1", device::DEVICE_TYPE::DT_DO, device::DEVICE_SUB_TYPE::DST_DO_VIRT );
@@ -27,22 +35,31 @@ TEST( open_seat_action, init )
 	auto operation_state = operation[ 0 ][ 1 ];
 	auto step = operation_state[ 0 ][ 1 ];
 
-	auto action_upper = step[ 0 ][ step::ACTIONS::A_UPPER_SEATS_ON ];
-	action_upper->add_dev( &test_DO, 0, valve::V_UPPER_SEAT );
-	action_upper->add_dev( &test_v_mix_proof, 0, valve::V_UPPER_SEAT );
-	action_upper->init();
-	auto action_lower = step[ 0 ][ step::ACTIONS::A_LOWER_SEATS_ON ];
-	action_lower->add_dev( &test_DO, 0, valve::V_LOWER_SEAT );
-	action_lower->add_dev( &test_v_mix_proof, 0, valve::V_LOWER_SEAT );
-	action_lower->init();
+	auto action = step[ 0 ][ step::ACTIONS::A_UPPER_SEATS_ON ];
+	action->add_dev( &test_DO, 0, valve::V_UPPER_SEAT );
+	action->add_dev( &test_v_mix_proof, 0, valve::V_UPPER_SEAT );
+	action->add_dev( &test_DO, 0, valve::V_LOWER_SEAT );
+	action->add_dev( &test_v_mix_proof, 0, valve::V_LOWER_SEAT );
 
-	action_upper->evaluate();
-	action_upper->evaluate();
-	action_lower->evaluate();
-	action_lower->evaluate();
-	action_upper->final();
-	action_lower->final();
+	action->init();
 
+	action->evaluate();  // Wait
+	sleep_ms( 500 );
+	action->evaluate();
+
+	action->evaluate(); // Lower seats
+	sleep_ms( 100 );
+	action->evaluate();
+
+	action->evaluate();  // Wait
+	sleep_ms( 500 );
+	action->evaluate();
+
+	action->evaluate(); // Upper seats
+	sleep_ms( 100 );
+	action->evaluate();
+
+	action->final();
 
 	test_project_manager::removeObject();
 	test_params_manager::removeObject();
