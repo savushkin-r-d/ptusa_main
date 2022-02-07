1c17ff373e61e97335f6891bbaede27f3a1404fe
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2022-02-07 16:10:15 +0300

    Refactor tests

--- test/bus_coupler_io_tests.cpp
+++ test/bus_coupler_io_tests.cpp
@@ -9,14 +9,12 @@ using namespace ::testing;
 const int STD_IN_FILENO = 1;
 
 /*
-	TEST METHOD DEFENITION:
-	void io_manager::add_node( u_int index, int ntype, int address,
-		char* IP_address, char *name,
-		int DO_cnt, int DI_cnt,
-		int AO_cnt, int AO_size, int AI_cnt, int AI_size )
+	TEST METHODS DEFENITION:
+	void print()
+	void print_log()
 */
 
-TEST( io_manager, add_node )
+TEST( io_manager, print )
 	{
 	const int ZERO_SIZE = 0;
 	const int ZERO_COUNT = 0;
@@ -26,15 +24,7 @@ TEST( io_manager, add_node )
 		io_manager::io_node::PHOENIX_BK_ETH, 1, "127.0.0.1",
 		"Axxx", ZERO_COUNT, ZERO_COUNT,
 		ZERO_COUNT, ZERO_SIZE, ZERO_COUNT, ZERO_SIZE );
-	}
 
-/*
-	TEST METHOD DEFENITION:
-	void print()
-*/
-
-TEST( io_manager, print )
-	{
 	fflush( stdout );
 	auto old = dup( STD_IN_FILENO );
 	auto tmp_name = "out1.txt";
@@ -45,32 +35,6 @@ TEST( io_manager, print )
 		close( tmp );
 
 		io_manager::get_instance()->print();
-
-		fflush( stdout );
-		dup2( old, STD_IN_FILENO );
-		close( old );
-		}
-	struct stat st;
-	stat( tmp_name, &st );
-	EXPECT_GT( st.st_size, 0 );
-	}
-
-/*
-	TEST METHOD DEFENITION:
-	void print_log()
-*/
-
-TEST( io_manager, print_log )
-	{
-	fflush( stdout );
-	auto old = dup( STD_IN_FILENO );
-	auto tmp_name = "out2.txt";
-	auto tmp = open( tmp_name, O_CREAT | O_WRONLY | O_TRUNC );
-	if ( tmp > 0 )
-		{
-		dup2( tmp, STD_IN_FILENO );
-		close( tmp );
-
 		io_manager::get_instance()->print_log();
 
 		fflush( stdout );
