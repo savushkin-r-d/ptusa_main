1d4e0f20f074a629b9f6cf8e0e65c689e582dcdc
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2022-02-11 16:50:07 +0300

    Refactor test

--- test/PAC_info_tests.cpp
+++ test/PAC_info_tests.cpp
@@ -3,12 +3,12 @@
 using namespace ::testing;
 
 /*
-	TEST METHOD DEFENITION:
-	void evaluate()
+    TEST METHOD DEFENITION:
+    int set_cmd( const char *prop, u_int idx, double val )
 */
 
 TEST( PAC_info, set_cmd )
-	{
+    {
     auto res = G_PAC_INFO()->set_cmd( "CMD", 0, PAC_info::RELOAD_RESTRICTIONS );
-	EXPECT_EQ( 0, res );
-	}
+    EXPECT_EQ( 0, res );
+    }
