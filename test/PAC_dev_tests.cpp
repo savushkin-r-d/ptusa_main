9f58c031cd3400c2dbcc06789fdfe567efe00402
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2022-02-11 09:26:06 +0300

    Add test

--- test/PAC_dev_tests.cpp
+++ test/PAC_dev_tests.cpp
@@ -28,3 +28,29 @@ TEST( signal_column, turn_off_blue )
 	test_dev.turn_off_blue();
 	EXPECT_EQ( false, test_dev.is_blue_switched_on() );
 	}
+
+TEST( valve_iolink_vtug, get_state_data )
+	{
+	class valve_iolink_vtug_test : public valve_iolink_vtug
+		{
+		public:
+			valve_iolink_vtug_test( const char* dev_name,
+				device::DEVICE_SUB_TYPE sub_type ) :valve_iolink_vtug(
+				dev_name, sub_type ) {};
+
+			/// @brief Получение данных состояния устройства.
+			char get_state_data( char* data )
+				{
+				return valve_iolink_vtug::get_state_data( data );
+				}
+
+		};
+
+	valve_iolink_vtug_test v( "V1", device::DEVICE_SUB_TYPE::V_IOLINK_VTUG_DO1 );
+
+	EXPECT_EQ( 0, v.get_state_data( nullptr ) );
+	char state = 1;
+	EXPECT_EQ( 0, v.get_state_data( &state ) );
+	v.set_rt_par( 1, 1 );							//Set "vtug_number" to 1.
+	EXPECT_EQ( 1, v.get_state_data( &state ) );
+	}
