2f7341c845f9a4f173aa873387a24414726cef6a
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2022-03-29 10:41:22 +0300

    Add tests

--- test/PAC_dev_tests.cpp
+++ test/PAC_dev_tests.cpp
@@ -98,3 +98,13 @@ TEST( device_manager, add_io_device )
         device::DT_TE, device::DST_TE_ANALOG, "T1", "Test sensor", "T" );
     EXPECT_NE( nullptr, res );
     }
+
+
+TEST( device, save_device )
+    {
+    temperature_e_analog t1( "T1" );
+    const int BUFF_SIZE = 100;
+    char buff[ BUFF_SIZE ] = { 0 };
+    t1.save_device( buff, "" );
+    EXPECT_STREQ( "T1={M=0, ST=1, V=0, P_CZ=0, P_ERR_T=0, P_MIN_V=0, P_MAX_V=0},\n", buff );
+    }
