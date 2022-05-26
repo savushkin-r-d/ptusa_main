5d510ed1c2a56d76bef5eccee87270f5b0193c90
Yaroslav Ozimok <102078275+YarickBSTU@users.noreply.github.com>
2022-05-26 13:34:27 +0300

    fixxes

--- PAC/IoT/IOT_dev_lua.cpp
+++ PAC/IoT/IOT_dev_lua.cpp
@@ -1,6 +1,6 @@
 /*
 ** Lua binding: IOT_dev
-** Generated automatically by tolua++-1.0.92 on 02/10/22 15:18:55.
+** Generated automatically by tolua++-1.0.92 on Thu May 26 13:25:49 2022.
 */
 
 #ifndef __cplusplus
