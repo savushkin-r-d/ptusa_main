cf8f7536811791c3f7aed42e1b595f6eff03e6c9
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2022-06-07 12:18:53 +0300

    Correct action switching off (#436)
    * Correct action switching off
    
    * Add tests
    
    * Remove outdated test
    
    * Refactor tests

--- test/mode_mngr_tests.cpp
+++ test/mode_mngr_tests.cpp
@@ -324,3 +324,93 @@ TEST( delay_on_action, evaluate )
 
 	test_params_manager::removeObject();
 	}
+
+TEST( required_DI_action, final )
+	{
+	DI1 test_DI( "test_DI1", device::DEVICE_TYPE::DT_DI,
+		device::DEVICE_SUB_TYPE::DST_DI_VIRT, 0 );
+	auto action = checked_devices_action();
+	action.add_dev( &test_DI );
+
+	test_DI.on();
+	action.init();
+	action.evaluate();
+	EXPECT_EQ( 1, test_DI.get_state() );
+
+	action.final();
+	EXPECT_EQ( 1, test_DI.get_state() );
+	}
+
+TEST( DI_DO_action, final )
+	{
+	DO1 test_DO( "test_DO1", device::DEVICE_TYPE::DT_DO,
+		device::DEVICE_SUB_TYPE::DST_DO_VIRT );
+	DI1 test_DI( "test_DI1", device::DEVICE_TYPE::DT_DI,
+		device::DEVICE_SUB_TYPE::DST_DI_VIRT, 0 );
+	auto action = DI_DO_action();
+	action.add_dev( &test_DI, action::MAIN_GROUP, 1 );
+	action.add_dev( &test_DO, action::MAIN_GROUP, 1 );
+
+	test_DI.on();
+	action.init();
+	action.evaluate();
+	EXPECT_EQ( 1, test_DI.get_state() );
+	EXPECT_EQ( 1, test_DO.get_state() );
+
+	action.final();
+	EXPECT_EQ( 1, test_DI.get_state() );
+	EXPECT_EQ( 0, test_DO.get_state() );
+	}
+
+TEST( AI_AO_action, final )
+	{
+	DO1 test_AO( "test_AO1", device::DEVICE_TYPE::DT_AO,
+		device::DEVICE_SUB_TYPE::DST_AO_VIRT );
+	DI1 test_AI( "test_AI1", device::DEVICE_TYPE::DT_AI,
+		device::DEVICE_SUB_TYPE::DST_AI_VIRT, 0 );
+	auto action = AI_AO_action();
+	action.add_dev( &test_AI, action::MAIN_GROUP, 1 );
+	action.add_dev( &test_AO, action::MAIN_GROUP, 1 );
+
+	const int VALUE = 50;
+	test_AI.set_value( VALUE );
+	action.init();
+	action.evaluate();
+	EXPECT_EQ( VALUE, test_AI.get_value() );
+	EXPECT_EQ( VALUE, test_AO.get_value() );
+
+	action.final();
+	EXPECT_EQ( VALUE, test_AI.get_value() );
+	EXPECT_EQ( 0, test_AO.get_value() );
+	}
+
+TEST( wash_action, final )
+	{
+	DO1 test_DO( "test_DO1", device::DEVICE_TYPE::DT_DO,
+		device::DEVICE_SUB_TYPE::DST_DO_VIRT );
+	DI1 test_DI( "test_DI1", device::DEVICE_TYPE::DT_DI,
+		device::DEVICE_SUB_TYPE::DST_DI_VIRT, 0 );
+
+	virtual_motor test_M1( "M1" );
+	virtual_motor test_M2( "M2" );
+
+	auto action = wash_action();
+	action.add_dev( &test_DI, action::MAIN_GROUP, 0 );
+	action.add_dev( &test_DO, action::MAIN_GROUP, 1 );
+	action.add_dev( &test_M1, action::MAIN_GROUP, 2 );
+	action.add_dev( &test_M2, action::MAIN_GROUP, 3 );
+
+	test_DI.on();
+	action.init();
+	action.evaluate();
+	EXPECT_EQ( 1, test_DI.get_state() );
+	EXPECT_EQ( 1, test_DO.get_state() );
+	EXPECT_EQ( 1, test_M1.get_state() );
+	EXPECT_EQ( 2, test_M2.get_state() );
+
+	action.final();
+	EXPECT_EQ( 1, test_DI.get_state() );
+	EXPECT_EQ( 0, test_DO.get_state() );
+	EXPECT_EQ( 0, test_M1.get_state() );
+	EXPECT_EQ( 0, test_M2.get_state() );
+	}
