cc34fe9eab902ae5bfbf14ab26615e2f08d36356
Alexa <neroda-01@mail.ru>
2022-09-15 10:46:13 +0300

    Add test files for windows configuration

---
+++ test/w_tcp_cmctr_test.cpp
@@ -0,0 +1,8 @@
+#include "w_tcp_cmctr_test.h"
+
+using namespace ::testing;
+
+#ifdef WIN_OS
+
+
+#endif
---
+++ test/w_tcp_cmctr_test.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "includes.h"
+
+#ifdef WIN_OS
+#include "w_tcp_cmctr.h"
+#include "w_tcp_client.h"
+#endif
