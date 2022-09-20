70630b4071dc478861ea53154e38153c82a67e54
Alexa <neroda-01@mail.ru>
2022-09-20 14:32:04 +0300

    Сhanged the encoding of the file

--- test/w_tcp_cmctr_test.cpp
+++ test/w_tcp_cmctr_test.cpp
@@ -5,19 +5,18 @@ using namespace ::testing;
 #ifdef WIN_OS
 TEST( tcp_communicator_win, evaluate )
 {
-    G_CMMCTR->init_instance("Òåñò", "Test");
+    G_CMMCTR->init_instance("Ð¢ÐµÑÑ‚", "Test");
     EXPECT_EQ(0, G_CMMCTR->evaluate());
 
     win_tcp_client cl("127.0.0.1", 10000, 1, 1); 
     cl.Connect();
 
-    EXPECT_EQ(0, G_CMMCTR->evaluate());
+    EXPECT_EQ(0,G_CMMCTR->evaluate());
 
     cl.buff[0] = 33;
     cl.buff[1] = 33;
     cl.AsyncSend(2);
 
-    //sleep(0); 
     EXPECT_EQ(0, G_CMMCTR->evaluate());
 
     EXPECT_EQ(0, G_CMMCTR->evaluate());
