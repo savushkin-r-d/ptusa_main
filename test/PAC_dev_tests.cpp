c319bff11669582cdfd3801b35467a16f08b96c9
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2022-09-21 10:07:21 +0300

    Add tests

--- test/PAC_dev_tests.cpp
+++ test/PAC_dev_tests.cpp
@@ -772,3 +772,29 @@ TEST( wages_RS232, evaluate_io )
     wages_RS232 w1( "W1" );
     w1.evaluate_io();
     }
+
+TEST( threshold_regulator, direct_set_value )
+    {
+    threshold_regulator p1( "test" );
+    device* p1_dev = static_cast<device*>( &p1 );
+
+
+    G_DEVICE_MANAGER()->clear_io_devices();
+    auto res = G_DEVICE_MANAGER()->add_io_device(
+        device::DT_TE, device::DST_TE_VIRT, "TE1", "Test sensor", "T" );
+    ASSERT_EQ( nullptr, res );
+    res = G_DEVICE_MANAGER()->add_io_device(
+        device::DT_M, device::DST_M_VIRT, "M1", "Test motor", "M" );
+    ASSERT_EQ( nullptr, res );
+    G_DEVICE_MANAGER()->print();
+
+    auto m1 = G_DEVICE_MANAGER()->get_M( "M1" );
+    ASSERT_NE( nullptr, m1 );
+    p1.set_string_property( "IN_VALUE", "TE1" );
+    p1.set_string_property( "OUT_VALUE", "M1" );
+
+    //Regulator switched off.
+    p1_dev->set_value( 10 );
+    EXPECT_EQ( .0f, m1->get_value() );
+    EXPECT_EQ( .0f, m1->get_state() );
+    }
