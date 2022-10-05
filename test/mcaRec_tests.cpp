878ef3314ce19e8a54c5164104bd1a21a7b35c63
Alexa <neroda-01@mail.ru>
2022-10-05 12:36:04 +0300

    test for ParentRecipeManager

--- test/mcaRec_tests.cpp
+++ test/mcaRec_tests.cpp
@@ -2,65 +2,66 @@
 
 using namespace ::testing;
 
-TEST( TMediumRecipeManager, LoadFromFile )
-    {
-    TMediumRecipeManager mngr( TMediumRecipeManager::MT_CAUSTIC );
+TEST(TMediumRecipeManager, LoadFromFile)
+{
+    TMediumRecipeManager mngr(TMediumRecipeManager::MT_CAUSTIC);
 
     auto recipeNo = 0;
-    mngr.ResetRecipeToDefaults( recipeNo );
+    mngr.ResetRecipeToDefaults(recipeNo);
     auto fileName = "tmp1.txt";
-    EXPECT_EQ( 0, mngr.SaveToFile( fileName ) );
-    EXPECT_EQ( 0, mngr.LoadFromFile( fileName ) );
-    }
+    EXPECT_EQ(0, mngr.SaveToFile(fileName));
+    EXPECT_EQ(0, mngr.LoadFromFile(fileName));
+}
 
-TEST( TMediumRecipeManager, CopyRecipe )
-    {
-    TMediumRecipeManager mngr( TMediumRecipeManager::MT_CAUSTIC );
+TEST(TMediumRecipeManager, CopyRecipe)
+{
+    TMediumRecipeManager mngr(TMediumRecipeManager::MT_CAUSTIC);
     mngr.CopyRecipe();
-    }
+}
 
-TEST( TMediumRecipeManager, PasteRecipe )
-    {
-    TMediumRecipeManager mngr( TMediumRecipeManager::MT_CAUSTIC );
+TEST(TMediumRecipeManager, PasteRecipe)
+{
+    TMediumRecipeManager mngr(TMediumRecipeManager::MT_CAUSTIC);
     mngr.PasteRecipe();
-    }
+}
 
-TEST( TMediumRecipeManager, NullifyRecipe )
-    {
-    TMediumRecipeManager mngr( TMediumRecipeManager::MT_CAUSTIC );
+TEST(TMediumRecipeManager, NullifyRecipe)
+{
+    TMediumRecipeManager mngr(TMediumRecipeManager::MT_CAUSTIC);
     mngr.NullifyRecipe();
-    }
+}
 
 
-TEST( TRecipeManager, LoadFromFile )
-    {
+TEST(TRecipeManager, LoadFromFile)
+{
     auto lineNo = 1;
-    TRecipeManager mngr( lineNo );
+    TRecipeManager mngr(lineNo);
 
     auto recipeNo = 0;
-    mngr.ResetRecipeToDefaults( recipeNo );
+    mngr.ResetRecipeToDefaults(recipeNo);
     auto fileName = "tmp2.txt";
-    EXPECT_EQ( 0, mngr.SaveToFile( fileName ) );
-    EXPECT_EQ( 0, mngr.LoadFromFile( fileName ) );
-    }
+    EXPECT_EQ(0, mngr.SaveToFile(fileName));
+    EXPECT_EQ(0, mngr.LoadFromFile(fileName));
+}
 
-TEST( TRecipeManager, CopyRecipe )
-    {
-    auto lineNo = 1;
-    TRecipeManager mngr( lineNo );
+TEST(ParentRecipeManager, CopyRecipe) //тест прошел
+{
+    auto fortesting = 1;
+    ParentRecipeManager mngr(fortesting);
     mngr.CopyRecipe();
-    }
+}
 
-TEST( TRecipeManager, PasteRecipe )
-    {
-    auto lineNo = 1;
-    TRecipeManager mngr( lineNo );
+TEST(ParentRecipeManager, PasteRecipe)//тест пройден
+{
+    auto fortesting = 1;
+    ParentRecipeManager mngr(fortesting);
     mngr.PasteRecipe();
-    }
+}
 
-TEST( TRecipeManager, NullifyRecipe )
-    {
-    auto lineNo = 1;
-    TRecipeManager mngr( lineNo );
+TEST(ParentRecipeManager, NullifyRecipe)//тест прошел
+{
+    auto fortesting = 1;
+    ParentRecipeManager mngr(fortesting);
     mngr.NullifyRecipe();
-    }
+}
+
