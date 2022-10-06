afd75bd24fa4058e6eab6df1d40661848ecc8063
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2022-10-06 13:32:18 +0300

    Refactor code
    Remove compiler warning, fix some code smells.

--- PAC/IoT/iot_wages_eth.cpp
+++ PAC/IoT/iot_wages_eth.cpp
@@ -1,51 +1,51 @@
 #include "iot_wages_eth.h"
 #include "tcp_client.h"
 
-iot_wages_eth::iot_wages_eth( unsigned int id, char* ip, unsigned int port )
-	{
-	tc = tcp_client::Create( ip, port, id, 0 , 256u, 300u); 
-	}
+iot_wages_eth::iot_wages_eth( unsigned int id, char* ip, unsigned int port ) :
+    tc( std::unique_ptr<tcp_client>( tcp_client::Create( ip, port, id, 0,
+    static_cast<unsigned int> ( CONSTANTS::BUFF_SIZE ), 300u ) ) )
+    {
+    }
 
 void iot_wages_eth::evaluate()
-	{
-	status = tc->Communicate( 0 );
-	convert_value();
-	}
+    {
+    status = tc->Communicate( 0 );
+    convert_value();
+    }
 
 int iot_wages_eth::get_wages_state() const
-	{
-	return state;
-	}
+    {
+    return state;
+    }
 
 float iot_wages_eth::get_wages_value() const
-	{
-	return value;
-	}
+    {
+    return value;
+    }
 
 void iot_wages_eth::set_wages_value( float new_value )
-	{
-	if ( new_value >= .0f ) value = new_value;
-	}
+    {
+    if ( new_value >= .0f ) value = new_value;
+    }
 
-void iot_wages_eth::convert_value() 
-	{
-	if ( tc->buff[ 8 ] != 'k' || tc->buff[ 9 ] != 'g' || !status )
-		{
-		state = 0;
-		return;
-		}
-	else
-		{
-		state = 1;
-		value = static_cast<float>( atof( tc->buff + 1 ) );
-		}
-	}
+void iot_wages_eth::convert_value()
+    {
+    if ( tc->buff[ 8 ] != 'k' || tc->buff[ 9 ] != 'g' || !status )
+        {
+        state = 0;
+        return;
+        }
+    else
+        {
+        state = 1;
+        value = static_cast<float>( atof( tc->buff + 1 ) );
+        }
+    }
 
 void iot_wages_eth::direct_set_tcp_buff( char* new_value, int new_status )
-	{
-	strcpy( tc->buff, new_value );
-	status = new_status;
-	convert_value();
-	}
-
-
+    {
+    strncpy( tc->buff, new_value,
+        static_cast<unsigned int> ( CONSTANTS::BUFF_SIZE ) );
+    status = new_status;
+    convert_value();
+    }
--- PAC/IoT/iot_wages_eth.h
+++ PAC/IoT/iot_wages_eth.h
@@ -1,29 +1,35 @@
 #pragma once
 
 #include "iot_base.h"
-#include "smart_ptr.h"
+#include <memory>
+
 class tcp_client;
 
 class iot_wages_eth : public i_iot_node
-	{
-	public:
-		iot_wages_eth( unsigned int id, char* ip, unsigned int port );
+    {
+    public:
+        iot_wages_eth( unsigned int id, char* ip, unsigned int port );
 
-		void evaluate();
+        void evaluate();
 
-		int get_wages_state() const;
+        int get_wages_state() const;
 
-		float get_wages_value() const;
+        float get_wages_value() const;
 
-		void set_wages_value( float new_value );
+        void set_wages_value( float new_value );
 
-		void convert_value();
-		
-		void direct_set_tcp_buff( char* new_value, int new_status);
+        void convert_value();
 
-	private:
-		int status = 0;
-		int state = 0;
-		float value = .0f;
-		auto_smart_ptr < tcp_client > tc;
-	};
+        void direct_set_tcp_buff( char* new_value, int new_status );
+
+    private:
+        enum class CONSTANTS
+            {
+            BUFF_SIZE = 256,
+            };
+
+        int status = 0;
+        int state = 0;
+        float value = .0f;
+        std::unique_ptr < tcp_client > tc;
+    };
