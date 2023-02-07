e82681b51e6e65156037fbab3d06c042ed596d9f
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2023-02-07 14:59:42 +0300

    Fix directory name to correct testing

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -5,7 +5,7 @@ file(WRITE "@CMAKE_BINARY_DIR@/CTestTestfile.cmake" "")
 
 # loop through the lines,
 foreach(LINE IN LISTS LINES)
-  if(NOT "${LINE}" MATCHES [[subdirs\(\"lua\"\)|subdirs\(\"zlib\"\)|subdirs\(\"tolua\+\+\"\)|subdirs\(\"benchmark\"\)]] )
+  if(NOT "${LINE}" MATCHES [[subdirs\(\"lua\"\)|subdirs\(\"zlib\"\)|subdirs\(\"toluapp\"\)|subdirs\(\"benchmark\"\)]] )
     # write line without unwanted parts ...
     file(APPEND "@CMAKE_BINARY_DIR@/CTestTestfile.cmake" "${LINE}\n")
   endif()
