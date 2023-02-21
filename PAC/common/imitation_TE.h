34927a178d294612af21ec759103bc99327f695a
Yana Sitkovets <asitkovec@gmail.com>
2023-02-21 12:05:26 +0300

    Sonar suggestions

--- PAC/common/imitation_TE.h
+++ PAC/common/imitation_TE.h
@@ -2,24 +2,24 @@
 #define _USE_MATH_DEFINES
 #include <random>
 #include <cmath>
-
 class imitation_TE 
     {
     public:
-        imitation_TE();
-        imitation_TE(float D,float m_expec,float st_deviation);
+        imitation_TE() = default;
+        imitation_TE(float D,float m_expec);
         float get_TE(); // возвращает значения температуры 
 
     private:
-        float D = 0.234;   // дисперсия
+        float D = float(0.234);   // дисперсия
         float m_expec = 27; // мат. ожидание 
-        float st_deviation = 0.48; // стандартное отклонение 
-        float old_value = 25.7;
-        std::default_random_engine gen = random_gen();
+        float st_deviation = float(0.48); // стандартное отклонение 
+        float old_value = float(25.7);
+        std::random_device rd{};
+        std::mt19937 gen{ rd() };
         std::uniform_real_distribution<float> x_dis = std::uniform_real_distribution<float>(20, 30);  // функция генерирует случайные вещественные числа из заданного диапазона  
-        std::default_random_engine random_gen(); // возврат случайной вещественной величины
         float x = x_dis(gen);  // случайная величмина 
-        bool is_p();   // функция расчета вероятности 
-
+        const bool is_p();   // функция расчета вероятности 
+        const float get_st_deviation();
+       
     };
 
