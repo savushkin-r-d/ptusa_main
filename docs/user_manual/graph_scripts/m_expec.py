f8fa9e99bc9cde8a90d35861ed3483fd20932bc5
Yana Sitkovets <asitkovec@gmail.com>
2023-05-03 17:11:01 +0300

    Add graph_scripts

---
+++ docs/user_manual/graph_scripts/m_expec.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.stats import norm
+
+plt.style.use('dark_background') # задаем стииль окна
+
+fig, ax = plt.subplots() # фигура для отрисовки графика 
+x_min, x_max = 26, 30 # задаем диапозон случайной величины 
+mean = 27.9 # мат. ожидание 
+std = 0.5   # стандартное отклонение
+n = 200 # количестов элементов в linspace (массиве)
+x = np.linspace(x_min, x_max, n)  # последовательность чисел в заданном интервале 
+ax.set_ylim(bottom=0) # задаем начало оси y от 0
+ax.grid(alpha=0.8,color='b') # добавляем сетку с прозрачностью 0.8 и задаем цвет
+ax.vlines(x=mean, ymin=0, ymax=0.795, linewidths=2.0) # отрисовка линии по координатам x, y и верхней границе
+ax.text(28, 0.1, r'$\mu $', fontsize=20,color='w') # ставим символ мю на графике 
+ax.set_ylabel(r'$p$ (TE)') # называем ось y
+ax.set_xlabel('TE values') # называем ось x
+ax.plot(x, norm.pdf(x, loc=mean, scale=std)) # рисуем график 
---
+++ docs/user_manual/graph_scripts/stdev.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.stats import norm
+
+plt.style.use('dark_background') # задаем стииль окна
+
+fig, ax = plt.subplots() # фигура для отрисовки графика 
+x_min, x_max = 26, 30 # задаем диапозон случайной величины 
+mean = 27.9 # мат. ожидание 
+stdev = 0.5   # стандартное отклонение, да не 0.83, но так график по симпотичнее 
+n = 200 # количестов элементов в linspace (массиве)
+x = np.linspace(x_min, x_max, n)  # последовательность чисел в заданном интервале 
+ax.set_ylim(bottom=0) # задаем начало оси y от 0
+ax.grid(alpha=0.8,color='b') # добавляем сетку с прозрачностью 0.8 и задаем цвет
+ax.vlines(x=mean, ymin=0, ymax=0.795, linewidths=2.0) # отрисовка линии по координатам x, y и верхней границе
+ax.set_ylabel(r'$p$ (TE)') # называем ось y
+ax.set_xlabel('TE values') # называем ось x
+ax.plot(x, norm.pdf(x, loc=mean, scale=stdev)) # рисуем график 
+
+mu_plus_stdev = mean + 0.83
+mu_subtract_stdev = mean - 0.83
+px=np.arange(mu_plus_stdev, mu_subtract_stdev, 0.01)
+ax.fill_between(px,norm.pdf(px, loc=mean, scale=stdev), alpha=0.5, color='blue')
+pro=norm(mean, stdev).cdf(mu_subtract_stdev) - norm(mean, stdev).cdf(mu_plus_stdev)
+ax.text(27.4, 0.1, r'$\mu $ - 0.83', fontsize=15, color='w')
+ax.text(28, 0.1,r'$\mu $ + 0.83', fontsize=15, color='w')
+plt.show() # метод для показа графика, без него никак
