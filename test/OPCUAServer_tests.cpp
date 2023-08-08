d6706b0b25d86c0def7b71858b93eef1777471f4
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2023-08-08 16:32:45 +0300

    Refactor test

--- test/OPCUAServer_tests.cpp
+++ test/OPCUAServer_tests.cpp
@@ -8,9 +8,27 @@ TEST( OPCUAServer, Evaluate )
     G_DEVICE_MANAGER()->add_io_device( device::DT_V, device::DST_V_DO1,
         "Valve1", "Test Valve", "" );
     OPCUAServer::getInstance().CreateDevObjects();
-    EXPECT_EQ( UA_STATUSCODE_GOOD, OPCUAServer::getInstance().Start() );
+
+    auto res = OPCUAServer::getInstance().Start();
+    EXPECT_EQ( UA_STATUSCODE_GOOD, res  );
 
     OPCUAServer::getInstance().Evaluate();
+    
+    res = OPCUAServer::getInstance().readState( nullptr, nullptr, nullptr,
+        nullptr, nullptr, false, nullptr, nullptr );
+    EXPECT_EQ( UA_STATUSCODE_GOOD, res );
+
+    res = OPCUAServer::getInstance().writeState( nullptr, nullptr, nullptr,
+        nullptr, nullptr, nullptr, nullptr );
+    EXPECT_EQ( UA_STATUSCODE_GOOD, res );
+
+    res = OPCUAServer::getInstance().readValue( nullptr, nullptr, nullptr,
+        nullptr, nullptr, false, nullptr, nullptr );
+    EXPECT_EQ( UA_STATUSCODE_GOOD, res );
+
+    res = OPCUAServer::getInstance().writeValue( nullptr, nullptr, nullptr,
+        nullptr, nullptr, nullptr, nullptr );
+    EXPECT_EQ( UA_STATUSCODE_GOOD, res );
 
     OPCUAServer::getInstance().Shutdown();
     }
