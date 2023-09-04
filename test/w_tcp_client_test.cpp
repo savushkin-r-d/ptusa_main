56e1125efc40866640ea94ffbc00b4b082b9d8cd
Yaroslav Ozimok <102078275+YarickBSTU@users.noreply.github.com>
2023-09-04 15:29:21 +0300

    added tests

---
+++ test/w_tcp_client_test.cpp
@@ -0,0 +1,14 @@
+#include "w_tcp_client_test.h"
+
+using namespace ::testing;
+
+TEST( tcp_client, checkConnection )
+	{
+	win_tcp_client cl( "127.0.0.1", 10000, 1, 1 );
+
+	EXPECT_EQ( 0, cl.checkConnection() );
+
+	cl.reconnectTimeout = 0;
+	 
+	EXPECT_EQ( 0, cl.checkConnection() );
+	}
---
+++ test/w_tcp_client_test.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "includes.h"
+
+#include "w_tcp_client.h"
