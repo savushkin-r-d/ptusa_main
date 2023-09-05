bfeaeb6a215d561c402870a50f9c200e884decde
Yaroslav Ozimok <102078275+YarickBSTU@users.noreply.github.com>
2023-09-05 09:36:50 +0300

    test fix

--- test/w_tcp_client_test.cpp
+++ test/w_tcp_client_test.cpp
@@ -5,12 +5,13 @@ using namespace ::testing;
 #ifdef WIN_OS
 TEST( tcp_client, checkConnection )
 	{
-	win_tcp_client cl( "127.0.0.1", 10000, 1, 1 );
+	win_tcp_client cl( "127.0.0.0", 10000, 1, 1 );
 
 	EXPECT_EQ( 0, cl.checkConnection() );
 
-	cl.reconnectTimeout = 0;
-	 
+	cl.reconnectTimeout = 1000000;
+
 	EXPECT_EQ( 0, cl.checkConnection() );
+
 	}
 #endif
