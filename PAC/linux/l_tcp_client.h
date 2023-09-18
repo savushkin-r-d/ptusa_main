82112ad97e348447d78cd6c812be7b3cf043e20d
Yaroslav Ozimok <102078275+YarickBSTU@users.noreply.github.com>
2023-09-18 15:28:05 +0300

    Update PAC/linux/l_tcp_client.h
    Co-authored-by: Yana Sitkovets  <yana.sitkowecz@yandex.ru>

--- PAC/linux/l_tcp_client.h
+++ PAC/linux/l_tcp_client.h
@@ -23,7 +23,7 @@ class linux_tcp_client: public tcp_client
         virtual int AsyncSend( unsigned int bytestosend );
         int AsyncRecive() override;
         virtual int get_async_result() override;
-        linux_tcp_client(const char* client_ip, unsigned int client_port, unsigned int client_id, unsigned char alarm_subclass, unsigned int exchange_buf_size = 256, unsigned long send_receive_timeout = 100);
+        linux_tcp_client( const char* client_ip, unsigned int client_port, unsigned int client_id, unsigned char alarm_subclass, unsigned int exchange_buf_size = 256, unsigned long send_receive_timeout = 100 );
         ~linux_tcp_client();
 
     };
