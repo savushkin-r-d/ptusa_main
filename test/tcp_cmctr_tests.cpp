68c4860e3d006e20dd58ae885d8b9ad5242df5e4
Vasili Samuilik <Vasiliysamuilik@gmail.com>
2023-12-05 15:51:31 +0300

    Add test

--- test/tcp_cmctr_tests.cpp
+++ test/tcp_cmctr_tests.cpp
@@ -1,5 +1,6 @@
 #include "tcp_cmctr_tests.h"
 #include "dtime.h"
+#include "fmt/chrono.h"
 
 using namespace ::testing;
 
@@ -8,4 +9,20 @@ TEST(tcp_communicator, get_millisec_test)
     unsigned long milliseconds = get_millisec();
 
     ASSERT_GE(milliseconds, 0);
+}
+
+TEST(tcp_communicator, local_time_test)
+{
+    auto now = std::chrono::system_clock::now();
+
+    auto now_time_t = std::chrono::system_clock::to_time_t(now);
+
+    auto timeInfo = fmt::localtime(now_time_t);
+
+    ASSERT_NE(timeInfo.tm_year, -1);
+    ASSERT_NE(timeInfo.tm_mon, -1);
+    ASSERT_NE(timeInfo.tm_mday, -1);
+    ASSERT_NE(timeInfo.tm_hour, -1);
+    ASSERT_NE(timeInfo.tm_min, -1);
+    ASSERT_NE(timeInfo.tm_sec, -1);
 }
