e7e7770e97d4066237ed3022038488f05981345a
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2024-01-24 13:43:08 +0300

    Improve test

--- test/prj_mngr_tests.cpp
+++ test/prj_mngr_tests.cpp
@@ -11,10 +11,49 @@ TEST( project_manager, lua_load_configuration )
     G_LUA_MANAGER->set_Lua( L );
 
 
-    // Should fail without correct data.
+    // Should fail without correct data (no system Lua-scripts).
     auto res = G_PROJECT_MANAGER->lua_load_configuration();
     ASSERT_EQ( 1, res );
-
    
+    // Should not fail - correct data. System scripts (Lua) return 0 -
+    // just everthing is OK.
+    auto system_Lua_script = R"(
+system =
+    {
+    create_io = function( self )
+        return 0  
+    end,
+
+    create_devices = function( self )
+        return 0
+    end,
+
+    init_devices_properties = function( self )
+        return 0
+    end,
+
+    init_devices_params = function( self )
+        return 0
+    end
+    }
+
+init_tech_objects = function( self )
+    return 0
+end
+
+object_manager =
+    {
+    --Получение количества пользовательских технологических объектов.
+    get_objects_count = function( self )
+        return 0
+    end
+    }
+)";
+    lua_manager::use_print_stack_traceback();
+    EXPECT_EQ( 0, luaL_dostring( L, system_Lua_script ) );
+    res = G_PROJECT_MANAGER->lua_load_configuration();
+    ASSERT_EQ( 0, res );
+
+
     G_LUA_MANAGER->free_Lua();    
     }
