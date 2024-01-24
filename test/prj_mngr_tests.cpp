27f734ffe5ab5d41c13ce7fa535172016850e8a2
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2024-01-24 14:23:48 +0300

    Refactor test

--- test/prj_mngr_tests.cpp
+++ test/prj_mngr_tests.cpp
@@ -31,16 +31,9 @@ system =
 
     // Should fail without correct data - no init_devices_properties() Lua-function.
     auto system_Lua_script_2 = R"(
-system =
-    {
-    create_io = function( self )
-        return 0  
-    end,
-
-    create_devices = function( self )
+system.create_devices = function( self )
         return 0
     end
-    }
 )";
     EXPECT_EQ( 0, luaL_dostring( L, system_Lua_script_2 ) );
     res = G_PROJECT_MANAGER->lua_load_configuration();
@@ -48,20 +41,9 @@ system =
 
     // Should fail without correct data - no init_tech_objects() Lua-function.
     auto system_Lua_script_3 = R"(
-system =
-    {
-    create_io = function( self )
-        return 0  
-    end,
-
-    create_devices = function( self )
-        return 0
-    end,
-
-    init_devices_properties = function( self )
+system.init_devices_properties = function( self )
         return 0
     end
-    }
 )";
     EXPECT_EQ( 0, luaL_dostring( L, system_Lua_script_3 ) );
     res = G_PROJECT_MANAGER->lua_load_configuration();
@@ -69,21 +51,6 @@ system =
 
     // Should fail without correct data - no get_objects_count() Lua-function.
     auto system_Lua_script_4 = R"(
-system =
-    {
-    create_io = function( self )
-        return 0  
-    end,
-
-    create_devices = function( self )
-        return 0
-    end,
-
-    init_devices_properties = function( self )
-        return 0
-    end
-    }
-
 init_tech_objects = function( self )
     return 0
 end
@@ -93,28 +60,9 @@ end
     res = G_PROJECT_MANAGER->lua_load_configuration();
     ASSERT_EQ( 1, res );
 
-    // Should not fail - correct data. System functions (Lua) return 0 -
+    // Should not fail - all system Lua-functions. They (Lua) return 0 -
     // just everthing is OK.
-    auto system_Lua_script = R"(
-system =
-    {
-    create_io = function( self )
-        return 0  
-    end,
-
-    create_devices = function( self )
-        return 0
-    end,
-
-    init_devices_properties = function( self )
-        return 0
-    end
-    }
-
-init_tech_objects = function( self )
-    return 0
-end
-
+    auto system_Lua_script_5 = R"(
 object_manager =
     {
     --Получение количества пользовательских технологических объектов.
@@ -123,7 +71,7 @@ object_manager =
     end
     }
 )";
-    EXPECT_EQ( 0, luaL_dostring( L, system_Lua_script ) );
+    EXPECT_EQ( 0, luaL_dostring( L, system_Lua_script_5 ) );
     res = G_PROJECT_MANAGER->lua_load_configuration();
     ASSERT_EQ( 0, res );
 
