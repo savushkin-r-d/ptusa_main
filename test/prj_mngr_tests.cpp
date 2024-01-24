54eff878e4c18420c766c58916e2a7b1f5af8eb8
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2024-01-24 14:17:25 +0300

    Improve test

--- test/prj_mngr_tests.cpp
+++ test/prj_mngr_tests.cpp
@@ -11,11 +11,89 @@ TEST( project_manager, lua_load_configuration )
     G_LUA_MANAGER->set_Lua( L );
 
 
-    // Should fail without correct data (no system Lua-scripts).
+    // Should fail without correct data - no all system Lua-functions.
     auto res = G_PROJECT_MANAGER->lua_load_configuration();
     ASSERT_EQ( 1, res );
-   
-    // Should not fail - correct data. System scripts (Lua) return 0 -
+
+    // Should fail without correct data - no create_devices() Lua-function.
+    auto system_Lua_script_1 = R"(
+system =
+    {
+    create_io = function( self )
+        return 0  
+    end
+    }
+)";
+    EXPECT_EQ( 0, luaL_dostring( L, system_Lua_script_1 ) );
+    res = G_PROJECT_MANAGER->lua_load_configuration();
+    ASSERT_EQ( 1, res );
+
+
+    // Should fail without correct data - no init_devices_properties() Lua-function.
+    auto system_Lua_script_2 = R"(
+system =
+    {
+    create_io = function( self )
+        return 0  
+    end,
+
+    create_devices = function( self )
+        return 0
+    end
+    }
+)";
+    EXPECT_EQ( 0, luaL_dostring( L, system_Lua_script_2 ) );
+    res = G_PROJECT_MANAGER->lua_load_configuration();
+    ASSERT_EQ( 1, res );
+
+    // Should fail without correct data - no init_tech_objects() Lua-function.
+    auto system_Lua_script_3 = R"(
+system =
+    {
+    create_io = function( self )
+        return 0  
+    end,
+
+    create_devices = function( self )
+        return 0
+    end,
+
+    init_devices_properties = function( self )
+        return 0
+    end
+    }
+)";
+    EXPECT_EQ( 0, luaL_dostring( L, system_Lua_script_3 ) );
+    res = G_PROJECT_MANAGER->lua_load_configuration();
+    ASSERT_EQ( 1, res );
+
+    // Should fail without correct data - no get_objects_count() Lua-function.
+    auto system_Lua_script_4 = R"(
+system =
+    {
+    create_io = function( self )
+        return 0  
+    end,
+
+    create_devices = function( self )
+        return 0
+    end,
+
+    init_devices_properties = function( self )
+        return 0
+    end
+    }
+
+init_tech_objects = function( self )
+    return 0
+end
+)";
+
+    EXPECT_EQ( 0, luaL_dostring( L, system_Lua_script_4 ) );
+    res = G_PROJECT_MANAGER->lua_load_configuration();
+    ASSERT_EQ( 1, res );
+
+    // Should not fail - correct data. System functions (Lua) return 0 -
     // just everthing is OK.
     auto system_Lua_script = R"(
 system =
@@ -29,10 +107,6 @@ system =
     end,
 
     init_devices_properties = function( self )
-        return 0
-    end,
-
-    init_devices_params = function( self )
         return 0
     end
     }
@@ -49,7 +123,6 @@ object_manager =
     end
     }
 )";
-    lua_manager::use_print_stack_traceback();
     EXPECT_EQ( 0, luaL_dostring( L, system_Lua_script ) );
     res = G_PROJECT_MANAGER->lua_load_configuration();
     ASSERT_EQ( 0, res );
