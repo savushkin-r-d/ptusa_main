7954ebe2bbcc16de9bebfc1dc42dd10206dd1901
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2024-02-07 09:26:17 +0300

    Fix code smells from Codacy

--- test/uni_bus_coupler_io_tests.cpp
+++ test/uni_bus_coupler_io_tests.cpp
@@ -64,54 +64,54 @@ TEST( uni_io_manager, net_init )
     io_manager::io_node node( io_manager::io_node::TYPES::PHOENIX_BK_ETH,
         1000, "127.0.0.1", "A100", 1, 1, 1, 1, 1, 1 );
 
-    subhook_t socket_hook = subhook_new( (void*)socket, (void*)fail_socket,
-        SUBHOOK_64BIT_OFFSET );
+    subhook_t socket_hook = subhook_new( reinterpret_cast<void*>( socket ),
+        reinterpret_cast<void*>( fail_socket ), SUBHOOK_64BIT_OFFSET );
     subhook_install( socket_hook );
     // Should fail - fail with create socket.
     EXPECT_EQ( 3, mngr.net_init( &node ) );
     subhook_remove( socket_hook );
 
-    subhook_t setsockopt_hook = subhook_new( (void*)setsockopt, (void*)fail_setsockopt,
-        SUBHOOK_64BIT_OFFSET );
+    subhook_t setsockopt_hook = subhook_new( reinterpret_cast<void*>( setsockopt ),
+        reinterpret_cast<void*>( fail_setsockopt ), SUBHOOK_64BIT_OFFSET );
     subhook_install( setsockopt_hook );
     // Should fail - fail with setsockopt().
     EXPECT_EQ( 4, mngr.net_init( &node ) );
     subhook_remove( setsockopt_hook );
 
 #ifdef WIN_OS
-    subhook_t ioctlsocket_hook = subhook_new( (void*)ioctlsocket, (void*)fail_ioctlsocket,
-        SUBHOOK_64BIT_OFFSET );
+    subhook_t ioctlsocket_hook = subhook_new( reinterpret_cast<void*>( ioctlsocket ),
+        reinterpret_cast<void*>( fail_ioctlsocket ), SUBHOOK_64BIT_OFFSET );
     subhook_install( ioctlsocket_hook );
     // Should fail - fail with fcntl().
     EXPECT_EQ( 5, mngr.net_init( &node ) );
     subhook_remove( ioctlsocket_hook );
 #else
-    subhook_t fcntl_hook = subhook_new( (void*)fcntl, (void*)fail_fcntl,
-        SUBHOOK_64BIT_OFFSET );
+    subhook_t fcntl_hook = subhook_new( reinterpret_cast<void*>( fcntl ),
+        reinterpret_cast<void*>( fail_fcntl ), SUBHOOK_64BIT_OFFSET );
     subhook_install( fcntl_hook );
     // Should fail - fail with fcntl().
     EXPECT_EQ( 5, mngr.net_init( &node ) );
     subhook_remove( fcntl_hook );
 #endif
 
-    subhook_t select_m1_hook = subhook_new( (void*)select, (void*)fail_select_m1,
-        SUBHOOK_64BIT_OFFSET );
+    subhook_t select_m1_hook = subhook_new( reinterpret_cast<void*>( select ),
+        reinterpret_cast<void*>( fail_select_m1 ), SUBHOOK_64BIT_OFFSET );
     subhook_install( select_m1_hook );
     // Should fail - fail with select() - 1.
     EXPECT_EQ( 6, mngr.net_init( &node ) );
     subhook_remove( select_m1_hook );
-    subhook_t select_0_hook = subhook_new( (void*)select, (void*)fail_select_0,
-        SUBHOOK_64BIT_OFFSET );
+    subhook_t select_0_hook = subhook_new( reinterpret_cast<void*>( select ),
+        reinterpret_cast<void*>( fail_select_0 ), SUBHOOK_64BIT_OFFSET );
     subhook_install( select_0_hook );
     // Should fail - fail with select() - 0.
     EXPECT_EQ( 6, mngr.net_init( &node ) );
     subhook_remove( select_0_hook );
 
-    subhook_t getsockopt_1_hook = subhook_new( (void*)getsockopt, (void*)fail_getsockopt,
-        SUBHOOK_64BIT_OFFSET );
+    subhook_t getsockopt_1_hook = subhook_new( reinterpret_cast<void*>( getsockopt ),
+        reinterpret_cast<void*>( fail_getsockopt ), SUBHOOK_64BIT_OFFSET );
     subhook_install( getsockopt_1_hook );
-    subhook_t select_1_hook = subhook_new( (void*)select, (void*)success_select,
-        SUBHOOK_64BIT_OFFSET );
+    subhook_t select_1_hook = subhook_new( reinterpret_cast<void*>( select ),
+        reinterpret_cast<void*>( success_select ), SUBHOOK_64BIT_OFFSET );
     subhook_install( select_1_hook );
     // Should fail - fail with getsockopt() - 1.
     EXPECT_EQ( 7, mngr.net_init( &node ) );
@@ -119,8 +119,8 @@ TEST( uni_io_manager, net_init )
     subhook_remove( select_1_hook );
         
     subhook_install( select_1_hook );
-    subhook_t getsockopt_0_hook = subhook_new( (void*)getsockopt, (void*)success_getsockopt,
-        SUBHOOK_64BIT_OFFSET );
+    subhook_t getsockopt_0_hook = subhook_new( reinterpret_cast<void*>( getsockopt ),
+        reinterpret_cast<void*>( success_getsockopt ), SUBHOOK_64BIT_OFFSET );
     subhook_install( getsockopt_0_hook );
     // Should OK.
     EXPECT_EQ( 0, mngr.net_init( &node ) );
