7be8fb13e83af93203ff8c240e97ef345920323c
Yaroslav Ozimok <102078275+YaroslavOzimok@users.noreply.github.com>
2024-02-26 14:40:09 +0300

    Добавлена директива препроцессора #ifdef для теста OPCUa (#653)

--- test/PAC_info_tests.cpp
+++ test/PAC_info_tests.cpp
@@ -6,12 +6,13 @@ using namespace ::testing;
 TEST( PAC_info, set_cmd )
     {
     G_PAC_INFO()->set_cmd( "CMD", 0, PAC_info::RELOAD_RESTRICTIONS );
-
+#ifdef OPCUA
     EXPECT_EQ( 0, G_PAC_INFO()->par[ PAC_info::P_IS_OPC_UA_SERVER_ACTIVE ] );
     G_PAC_INFO()->set_cmd( "P_IS_OPC_UA_SERVER_ACTIVE", 0, 1 );
     EXPECT_EQ( 1, G_PAC_INFO()->par[ PAC_info::P_IS_OPC_UA_SERVER_ACTIVE ] );
     G_PAC_INFO()->set_cmd( "P_IS_OPC_UA_SERVER_ACTIVE", 0, 0 );
     EXPECT_EQ( 0, G_PAC_INFO()->par[ PAC_info::P_IS_OPC_UA_SERVER_ACTIVE ] );
+#endif
     }
 
 TEST( PAC_info, reset_params )
