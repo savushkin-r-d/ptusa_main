2997a166e71f85295de493efca099bc4058e01bf
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2024-03-19 09:46:10 +0300

    Fix message

--- PAC/common/prj_mngr.cpp
+++ PAC/common/prj_mngr.cpp
@@ -78,8 +78,7 @@ int project_manager::proc_main_params( int argc, const char* argv[] )
         if ( p > 0 ) 
             {
             tcp_communicator::set_port( p, p + 502 );
-            sprintf( G_LOG->msg, "New tcp_communicator ports: %d [modbus %d].", p, p + 502 );
-            G_LOG->write_log( i_log::P_NOTICE );
+            G_LOG->notice( "New tcp_communicator ports: %d [modbus %d].", p, p + 502 );
             }
         }
 
