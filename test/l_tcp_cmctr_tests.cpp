0337be00a69fe620f8d2bac77c9445f1ab36c54a
Yaroslav Ozimok <102078275+YaroslavOzimok@users.noreply.github.com>
2024-04-05 14:01:24 +0300

    Доработка тестов

--- test/l_tcp_cmctr_tests.cpp
+++ test/l_tcp_cmctr_tests.cpp
@@ -9,9 +9,14 @@ using namespace ::testing;
 #endif
 
 int __stdcall fail_recvtimeout( int, unsigned char*, int, long, long, const char *, const char *, stat_time*, char )
-{
+    {
     return -1;
-}
+    }
+
+int __stdcall checkbuff_true( int )
+    {
+    return 1;
+    }
 
 #ifdef LINUX_OS
 
@@ -44,13 +49,17 @@ TEST( tcp_communicator_linux, evaluate )
     subhook_t fail_recv = subhook_new( reinterpret_cast<void *>( tcp_communicator_linux::recvtimeout ),
         reinterpret_cast<void *>( fail_recvtimeout ), SUBHOOK_64BIT_OFFSET );
     subhook_install( fail_recv );
-    cl.async_timeout = 200;
-    cl.AsyncRecive();
-    cl.async_timeout = 0;
+    subhook_t checkb_true = subhook_new( reinterpret_cast<void *>( tcp_communicator_linux::checkBuff ),
+        reinterpret_cast<void *>( checkbuff_true ), SUBHOOK_64BIT_OFFSET );
+    subhook_install( fail_recv );
+    subhook_install( checkb_true );
     cl.AsyncRecive();
     subhook_remove( fail_recv );
     subhook_free( fail_recv );
-    fail_recvtimeout(0, nullptr, 0, 0, 0, nullptr, nullptr, nullptr, 0);
+    subhook_remove( checkb_true );
+    subhook_free( checkb_true );
+    fail_recvtimeout( 0, nullptr, 0, 0, 0, nullptr, nullptr, nullptr, 0 );
+    checkbuff_true( 0 );
 
     }
 
