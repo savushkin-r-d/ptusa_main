2aad531cfb134135c3171efd7506e5b7b78808a6
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2024-04-10 12:18:27 +0300

    Improve test

--- test/g_errors_tests.cpp
+++ test/g_errors_tests.cpp
@@ -6,9 +6,9 @@ using namespace ::testing;
 TEST( errors_manager, evaluate )
     {
     G_ERRORS_MANAGER->clear();
-    G_ERRORS_MANAGER->evaluate();    
+    G_ERRORS_MANAGER->evaluate();
     EXPECT_EQ( 0, G_ERRORS_MANAGER->get_errors_id() ); //No devices - no errors.
-    
+
     //Add counter to produce and check errors.    
     auto name = std::string( "FQT3" );
     auto res = G_DEVICE_MANAGER()->add_io_device(
@@ -22,7 +22,7 @@ TEST( errors_manager, evaluate )
     EXPECT_EQ( 0, G_ERRORS_MANAGER->get_errors_id() );
 
     //Generate error.
-    dev->set_cmd( "F", 0, 1 );    
+    dev->set_cmd( "F", 0, 1 );
     EXPECT_EQ( (int)i_counter::STATES::S_WORK, dev->get_state() );
     EXPECT_EQ( (int)i_counter::STATES::S_ERROR, dev->get_state() );
 
@@ -38,7 +38,7 @@ TEST( errors_manager, evaluate )
 
     //Should not get errors.
     G_ERRORS_MANAGER->evaluate();
-    EXPECT_EQ( 2, G_ERRORS_MANAGER->get_errors_id() );    
+    EXPECT_EQ( 2, G_ERRORS_MANAGER->get_errors_id() );
 
     //Should get a new error id due to set_cmd() with C_CMD_SUPPRESS.
     G_ERRORS_MANAGER->set_cmd( base_error::C_CMD_SUPPRESS, 7, 0, 1 );
@@ -52,6 +52,8 @@ TEST( errors_manager, evaluate )
     dev->set_cmd( "ABS_V", 0, 100 );
     dev->set_cmd( "P_DT", 0, 1000 );
     EXPECT_EQ( (int)i_counter::STATES::S_WORK, dev->get_state() );
-    G_ERRORS_MANAGER->evaluate();
+    G_ERRORS_MANAGER->evaluate(); //Process  ALARM_STATE::AS_ALARM -> AS_RETURN.
+    EXPECT_EQ( 4, G_ERRORS_MANAGER->get_errors_id() );
+    G_ERRORS_MANAGER->evaluate(); //Process  ALARM_STATE::AS_RETURN.
     EXPECT_EQ( 4, G_ERRORS_MANAGER->get_errors_id() );
     }
