6eb3e39b80a9eba9ca10b498ca5017d72feea493
Vasili Samuilik <Vasiliysamuilik@gmail.com>
2024-07-09 08:58:23 +0300

    Edit target and delete "MINIOS7"

--- PAC/MINIOS7/mos7_console.cpp
+++
@@ -1,7 +0,0 @@
-#include "mos7_console.h"
-
-int kb_hit() { return 0; }
-
-int get_char() {
-  return 0;  // В режиме эмуляции просто выводим на консоль сообщение.
-}
--- PAC/MINIOS7/mos7_console.h
+++
@@ -1,7 +0,0 @@
-#ifdef UPAC_7186E
-#include "7186e.h"
-#endif  // UPAC_7186E
-
-#ifdef UPAC_5000
-#include "5000/uPAC5000.h"
-#endif  // uPAC5000
