d896be6977c7080d2add36747cbaf341ae77f332
Lenonim <ilja.ist.am.besten@mail.ru>
2024-10-21 12:30:42 +0300

    Дополнение к тесту device_communicator

--- test/g_device_tests.cpp
+++ test/g_device_tests.cpp
@@ -40,5 +40,10 @@ TEST( device_communicator, write_devices_states_service )
     G_DEVICE_CMMCTR->write_devices_states_service( cmd_size, data, out_data );
     EXPECT_EQ( 'x', out_data[ 0 ] );
 
+    unsigned char recman_data[IN_BUFF_SIZE] = " __RECMAN[1]:set_cmd( \"hello\", 1, 2.5 )";
+    recman_data[0] = device_communicator::CMD_EXEC_DEVICE_COMMAND;
+    G_DEVICE_CMMCTR->write_devices_states_service(cmd_size, recman_data, out_data);
+    EXPECT_EQ('x', out_data[0]);
+
     G_LUA_MANAGER->free_Lua();
     }
