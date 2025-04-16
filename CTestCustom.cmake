b83921f5b7091ea8865eab3aaecdb720f7f13540
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2025-04-16 14:16:31 +0300

    Disables testing for the benchmark library
    This change disables the testing of the benchmark library by setting BENCHMARK_ENABLE_TESTING to OFF. This prevents tests related to the benchmark library from being executed.
    The benchmark subdirectory are removed from the CTest test file generation.

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -5,7 +5,7 @@ file(WRITE "@CMAKE_BINARY_DIR@/CTestTestfile.cmake" "")
 
 # loop through the lines,
 foreach(LINE IN LISTS LINES)
-  if(NOT "${LINE}" MATCHES [[subdirs\(\"deps/lua\"\)|subdirs\(\"deps/zlib\"\)|subdirs\(\"deps/toluapp\"\)|subdirs\(\"deps/benchmark\"\)]] )
+  if(NOT "${LINE}" MATCHES [[subdirs\(\"deps/lua\"\)|subdirs\(\"deps/zlib\"\)|subdirs\(\"deps/toluapp\"\)]] )
     # write line without unwanted parts ...
     file(APPEND "@CMAKE_BINARY_DIR@/CTestTestfile.cmake" "${LINE}\n")
   endif()
