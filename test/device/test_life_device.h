d33b3f6e7eb168e6fb1753bf6ba43e5dd2552fe1
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2025-04-22 15:34:43 +0300

    Refactors life device tests to use unique pointers
    Modernizes the life device tests by replacing raw pointers with std::unique_ptr for memory management, reducing the risk of memory leaks and improving code clarity.

--- test/device/test_life_device.h
+++ test/device/test_life_device.h
@@ -15,25 +15,14 @@ class mock_DI_device : public device
 class lifebit_test : public ::testing::Test
     {
     protected:
-        life_device* lb_dev;
-        mock_DI_device* mock_dev;
+        std::unique_ptr<life_device> life_bit = std::make_unique<life_device>(
+            "TestDevice", device::DEVICE_SUB_TYPE::DST_LIFEBIT );
+        std::unique_ptr<mock_DI_device> mock_DI_dev{};
 
         void SetUp() override 
             {
-            mock_dev = new mock_DI_device();
-
-            lb_dev = new life_device( "TestDevice", device::DEVICE_SUB_TYPE::DST_LIFEBIT );
             // Устанавливаем mock устройство.
-            lb_dev->set_property( "DEV", mock_dev );
-            }
-
-        void TearDown() override
-            {
-            delete lb_dev;
-            lb_dev = nullptr;
-
-            delete mock_dev;
-            mock_dev = nullptr;
+            life_bit->set_property( "DEV", mock_DI_dev.get() );
             }
     };
 
@@ -50,75 +39,64 @@ class mock_AI_device : public device
 class lifecounter_test : public ::testing::Test
     {
     protected:
-        life_device* life_counter;
-        mock_AI_device* mock_AI_dev;
+        std::unique_ptr<life_device> life_counter = std::make_unique<life_device>(
+            "TestDevice", device::DEVICE_SUB_TYPE::DST_LIFECOUNTER );
+        std::unique_ptr<mock_AI_device> mock_AI_dev{};
 
         void SetUp() override
             {
-            mock_AI_dev = new mock_AI_device();
-
-            life_counter = new life_device( "TestDevice", device::DEVICE_SUB_TYPE::DST_LIFECOUNTER );
             // Устанавливаем mock устройство.
-            life_counter->set_property( "DEV", mock_AI_dev );
-            }
-
-        void TearDown() override
-            {
-            delete life_counter;
-            life_counter = nullptr;
-
-            delete mock_AI_dev;
-            mock_AI_dev = nullptr;
+            life_counter->set_property( "DEV", mock_AI_dev.get() );
             }
     };
 
 TEST_F( lifebit_test, EvaluateIO_NoDiDevice_NoAction ) 
     {
-    EXPECT_EQ( lb_dev->get_state(), 0 );   // Состояние должно быть нулевое.
+    EXPECT_EQ( life_bit->get_state(), 0 );   // Состояние должно быть нулевое.
     
-    lb_dev->evaluate_io();
-    EXPECT_EQ( lb_dev->get_state(), 0 );   // Состояние должно быть нулевое.
+    life_bit->evaluate_io();
+    EXPECT_EQ( life_bit->get_state(), 0 );   // Состояние должно быть нулевое.
 
-    lb_dev->set_property( "DI", nullptr ); // Убираем устройство.
-    lb_dev->evaluate_io();
-    EXPECT_EQ( lb_dev->get_state(), 0 );   // Состояние должно быть нулевое.
+    life_bit->set_property( "DI", nullptr ); // Убираем устройство.
+    life_bit->evaluate_io();
+    EXPECT_EQ( life_bit->get_state(), 0 );   // Состояние должно быть нулевое.
     }
 
 TEST_F( lifebit_test, EvaluateIO_StateChanged_DeviceActivated ) 
     {
-    EXPECT_CALL( *mock_dev, get_state() )
+    EXPECT_CALL( *mock_DI_dev, get_state() )
         .WillOnce( ::testing::Return( 1 ) ); // Состояние сигнала изменилось.
 
-    lb_dev->evaluate_io();
-    EXPECT_EQ( lb_dev->get_state(), 1 );     // Устройство должно активироваться.
+    life_bit->evaluate_io();
+    EXPECT_EQ( life_bit->get_state(), 1 );     // Устройство должно активироваться.
     }
 
 TEST_F( lifebit_test, EvaluateIO_TimerExceeded_DeviceDeactivated )
     {
-    EXPECT_CALL( *mock_dev, get_state() )
+    EXPECT_CALL( *mock_DI_dev, get_state() )
         .WillOnce( ::testing::Return( 1 ) ); // Состояние изменилось.
 
-    lb_dev->evaluate_io();                
-    EXPECT_EQ( lb_dev->get_state(), 1 );     // Устройство должно активироваться.
+    life_bit->evaluate_io();                
+    EXPECT_EQ( life_bit->get_state(), 1 );     // Устройство должно активироваться.
 
 
     // Эмулируем превышение времени.
     // Устанавливаем DT = 100 мс.
-    lb_dev->set_par( static_cast<u_int>( life_device::PARAM::DT ), 0, 100 );
-    EXPECT_CALL( *mock_dev, get_state() )
+    life_bit->set_par( static_cast<u_int>( life_device::PARAM::DT ), 0, 100 );
+    EXPECT_CALL( *mock_DI_dev, get_state() )
         .WillRepeatedly( ::testing::Return( 1 ) ); // Состояние не меняется.
 
     // Эмулируем время. Устанавливаем время так, чтобы таймер истек.
-    lb_dev->start_time -= 200;
-    lb_dev->evaluate_io();
-    EXPECT_EQ( lb_dev->get_state(), 0 ); // Устройство должно деактивироваться
+    life_bit->start_time -= 200;
+    life_bit->evaluate_io();
+    EXPECT_EQ( life_bit->get_state(), 0 ); // Устройство должно деактивироваться
 
 
-    EXPECT_CALL( *mock_dev, get_state() )
+    EXPECT_CALL( *mock_DI_dev, get_state() )
         .WillOnce( ::testing::Return( 0 ) ); // Состояние изменилось.
 
-    lb_dev->evaluate_io();
-    EXPECT_EQ( lb_dev->get_state(), 1 );     // Устройство должно активироваться.
+    life_bit->evaluate_io();
+    EXPECT_EQ( life_bit->get_state(), 1 );     // Устройство должно активироваться.
     }
 
 TEST_F( lifebit_test, save_device )
@@ -127,7 +105,7 @@ TEST_F( lifebit_test, save_device )
     std::array<char, 256> buffer = { 0 };
 
     // Вызываем метод save_device
-    auto bytes_written = lb_dev->save_device( buffer.data() );
+    auto bytes_written = life_bit->save_device( buffer.data() );
 
     // Проверяем, что метод вернул корректное количество записанных байт.
     EXPECT_GT( bytes_written, 0 );
@@ -137,36 +115,36 @@ TEST_F( lifebit_test, save_device )
 
 TEST_F( lifebit_test, SetStringProperty_SetsDIDevice )
     {
-    lb_dev->set_property( "DI", nullptr );
+    life_bit->set_property( "DI", nullptr );
     // Проверяем, что изначально dev равен nullptr
-    EXPECT_EQ( lb_dev->dev, nullptr );
+    EXPECT_EQ( life_bit->dev, nullptr );
 
     // Устанавливаем свойство "DI" с именем устройства
     const char* device_name = "MockDevice1";
-    G_DEVICE_MANAGER()->add_device( mock_dev,
+    G_DEVICE_MANAGER()->add_device(static_cast<device*>( mock_DI_dev.get() ),
         device::DEVICE_TYPE::DT_LIFE_DEVICE );
-    lb_dev->set_string_property( "DI", device_name );
+    life_bit->set_string_property( "DI", device_name );
 
     // Проверяем, что dev был установлен
-    EXPECT_EQ( lb_dev->dev, mock_dev );
+    EXPECT_EQ( life_bit->dev, mock_DI_dev.get() );
 
-    G_DEVICE_MANAGER()->remove_device( mock_dev->get_serial_n() );
+    G_DEVICE_MANAGER()->remove_device( mock_DI_dev->get_serial_n() );
     }
 
 TEST_F( lifebit_test, SetStringProperty_InvalidField_NoAction )
     {
     // Устанавливаем несуществующее поле
     const char* invalid_field = "INVALID";
-    lb_dev->set_string_property( invalid_field, "SomeValue" );
+    life_bit->set_string_property( invalid_field, "SomeValue" );
 
     // Проверяем, что dev не изменился
-    EXPECT_EQ( lb_dev->dev, mock_dev );
+    EXPECT_EQ( life_bit->dev, mock_DI_dev.get() );
     }
 
 TEST_F( lifebit_test, GetNameInLua_ReturnsCorrectName )
     {
     const char* expected_name = "TestDevice";
-    const char* lua_name = lb_dev->get_name_in_Lua();
+    const char* lua_name = life_bit->get_name_in_Lua();
     ASSERT_STREQ( lua_name, expected_name ) << "get_name_in_Lua() should"
         "return the correct device name.";
     }
