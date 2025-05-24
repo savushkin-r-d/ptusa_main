3d77fc329e180a72c6b0618c935419f61c1ccd07
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2025-05-24 16:19:21 +0300

    [config][misc] Updates AXCF2152 build configuration
    Updates the AXCF2152 build configuration to use the 2024 LTS release.
    This ensures the build process uses the latest LTS configuration.

--- misc/bi_all.sh
+++ misc/bi_all.sh
@@ -2,8 +2,7 @@
 
 SECONDS=0
 
-( cmake -D CFG_NAME:string="build-windows-AXCF2152-2022-LTS-Release" -D BUILD_NAME:string="build-windows-AXCF2152-2022-LTS-Release" -P ./misc/bi.cmake ) 2>&1 | ./misc/colorize.sh
-( cmake -D CFG_NAME:string="build-windows-AXCF1152-2022-LTS-Release" -D BUILD_NAME:string="build-windows-AXCF1152-2022-LTS-Release" -P ./misc/bi.cmake ) 2>&1 | ./misc/colorize.sh
+( cmake -D CFG_NAME:string="build-windows-AXCF2152-2024-LTS-Release" -D BUILD_NAME:string="build-windows-AXCF2152-2024-LTS-Release" -P ./misc/bi.cmake ) 2>&1 | ./misc/colorize.sh
 ( cmake -D CFG_NAME:string="windows-VS2022-default" -D BUILD_NAME:string="build-windows-VS2022-release" -P ./misc/bi.cmake ) 2>&1 | ./misc/colorize.sh
 
 duration=$SECONDS
