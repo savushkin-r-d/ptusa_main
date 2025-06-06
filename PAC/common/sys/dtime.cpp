4b0dd85b8d05b5a81143b4d11e75fcb2aa63f757
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2025-06-06 14:07:23 +0300

    [tests] Fixes I/O manager test (#951)

--- PAC/common/sys/dtime.cpp
+++ PAC/common/sys/dtime.cpp
@@ -3,7 +3,7 @@
 #ifdef PTUSA_TEST
 tm get_time_next_hour()
     {
-    static struct tm timeInfo_;
+    thread_local static struct tm timeInfo_;
 #ifdef LINUX_OS
     auto t_ = time( nullptr );
     localtime_r( &t_, &timeInfo_ );
@@ -13,6 +13,18 @@ tm get_time_next_hour()
 #endif // LINUX_OS
     timeInfo_.tm_hour++;
 
+    return timeInfo_;
+    }
+
+tm get_fixed_time()
+    {
+    thread_local static struct tm timeInfo_;
+    static const time_t FIXED_TIMESTAMP = 1741737600;  // 2025-03-12 00.00.00
+#ifdef LINUX_OS
+    gmtime_r( &FIXED_TIMESTAMP, &timeInfo_ );
+#else
+    gmtime_s( &timeInfo_, &FIXED_TIMESTAMP );
+#endif
     return timeInfo_;
     }
 #endif
