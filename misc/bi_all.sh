716fae0b08b1ef1c3cc15c9a2f948285af50944b
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2025-09-15 16:00:39 +0300

    Improves build script for parallel execution
    Refactors the build script to enable parallel execution of multiple build configurations. This significantly reduces the overall build time. It also adds prefix support for cmake execution and displays build progress with colorized output.

--- misc/bi_all.sh
+++ misc/bi_all.sh
@@ -1,19 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 SECONDS=0
 
 configs=(
-    "build-windows-AXCF2152-2024-LTS-Release build-windows-AXCF2152-2024-LTS-Release"    
-    "build-windows-AXCF3152-2024-LTS-Release build-windows-AXCF3152-2024-LTS-Release"
-    "windows-VS2022-default build-windows-VS2022-release"
-    "linux-default build-linux-Release wsl"
+    "build-windows-AXCF2152-2024-LTS-Release build-windows-AXCF2152-2024-LTS-Release AXCF2152"
+    "build-windows-AXCF3152-2024-LTS-Release build-windows-AXCF3152-2024-LTS-Release AXCF3152"
+    "windows-VS2022-default build-windows-VS2022-release windows"
+    "linux-default build-linux-Release linux wsl"
 )
 
-for config in "${configs[@]}"; do
-    read -r CFG_NAME BUILD_NAME PREFIX <<< "$config"
-    ( ${PREFIX:+$PREFIX } cmake -D CFG_NAME:string="$CFG_NAME" -D BUILD_NAME:string="$BUILD_NAME" -P ./misc/bi.cmake ) 2>&1 | ./misc/colorize.sh    
+pids=()
+for i in "${!configs[@]}"; do
+    config="${configs[$i]}"
+    read -r CFG_NAME BUILD_NAME SHORT_NAME PREFIX <<< "$config"
+    (
+        {
+            echo "Начало сборки: $CFG_NAME"
+            ${PREFIX:+$PREFIX } cmake -D CFG_NAME:string="$CFG_NAME" -D BUILD_NAME:string="$BUILD_NAME" -P ./misc/bi.cmake
+            echo "Завершение сборки: $CFG_NAME"
+        } | while IFS= read -r line; do echo -e "$((i+1))> [$SHORT_NAME]  $line"; done
+    ) 2>&1 | ./misc/colorize.sh &
+    pids+=($!)
+done
+
+for pid in "${pids[@]}"; do
+    wait $pid
 done
 
 duration=$SECONDS
 echo -e '\033[1;36m'
-echo "$((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
+echo "$((duration / 60)) минут и $((duration % 60)) секунд затрачено."
