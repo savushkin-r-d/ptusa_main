1c5c86c0ba4932253e3dee0954d2a0e18cd4fd8a
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2025-11-24 15:08:51 +0300

    Adds deleted assignment operator to class
    Adds the deleted assignment operator to the class to prevent accidental copying.
    This ensures that the class behaves as intended and avoids potential issues related to unintended object duplication.

--- test/includes.h
+++ test/includes.h
@@ -12,36 +12,38 @@ unsigned long get_delta_millisec( unsigned long time1 );
 
 extern const subhook_t  GTESTS_DELTA_MILLISEC_SUBHOOK;
 
-class DeltaMilliSecSubHooker 
+class DeltaMilliSecSubHooker
     {
     inline static unsigned long millitime;
 
-public:
+    public:
 
-    static unsigned long get_millisec()
-        {
-        return millitime;
-        }
+        static unsigned long get_millisec()
+            {
+            return millitime;
+            }
 
-    static void set_millisec( unsigned long millisec )
-        {
-        millitime = millisec;
-        subhook_install( GTESTS_DELTA_MILLISEC_SUBHOOK );
-        }
+        static void set_millisec( unsigned long millisec )
+            {
+            millitime = millisec;
+            subhook_install( GTESTS_DELTA_MILLISEC_SUBHOOK );
+            }
 
-    static void set_default_time() 
-        {
-        millitime = 1UL;
-        subhook_remove( GTESTS_DELTA_MILLISEC_SUBHOOK );
-        }
+        static void set_default_time()
+            {
+            millitime = 1UL;
+            subhook_remove( GTESTS_DELTA_MILLISEC_SUBHOOK );
+            }
 
-    explicit DeltaMilliSecSubHooker( unsigned long millisec ) 
-        {
-        DeltaMilliSecSubHooker::set_millisec( millisec );
-        }
+        explicit DeltaMilliSecSubHooker( unsigned long millisec )
+            {
+            DeltaMilliSecSubHooker::set_millisec( millisec );
+            }
 
-    ~DeltaMilliSecSubHooker()
-        {
-        DeltaMilliSecSubHooker::set_default_time();
-        }
+        ~DeltaMilliSecSubHooker()
+            {
+            DeltaMilliSecSubHooker::set_default_time();
+            }
+
+        DeltaMilliSecSubHooker& operator=( DeltaMilliSecSubHooker&& ) = delete;
     };
