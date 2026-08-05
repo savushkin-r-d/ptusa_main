f5bcdb80f7d33323077c1d7934653b3ff6f14802
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2026-08-05 16:30:47 +0300

    Adds test

--- test/params_ex_tests.cpp
+++ test/params_ex_tests.cpp
@@ -13,3 +13,16 @@ TEST( params_manager, evaluate )
     pm->save();
     pm->evaluate();
     }
+
+TEST( params_manager, get_params_data )
+    {
+    auto pm = params_manager::get_instance();
+    int start_pos;
+
+    // Запрос большего количества данных, чем есть в памяти. Результат должен
+    // быть `nullptr`.
+    auto data = pm->get_params_data(
+        static_cast<int>( params_manager::CONSTANTS::C_TOTAL_PARAMS_SIZE ) + 1,
+        start_pos );
+    EXPECT_EQ( data, nullptr );
+    }
