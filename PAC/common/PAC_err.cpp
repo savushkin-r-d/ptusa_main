35246317381a820921bf11847b23f98bdb8a9488
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2026-08-31 17:51:26 +0300

    Uses standard range-based for loop syntax

--- PAC/common/PAC_err.cpp
+++ PAC/common/PAC_err.cpp
@@ -179,7 +179,7 @@ int PAC_critical_errors_manager::save_as_Lua_str( char *str, u_int_2 &id )
 bool PAC_critical_errors_manager::is_critical_error()
     {
     is_any_is_critical_error = false;
-    for each( auto err in errors )
+    for( auto& err : errors )
         {
         if ( err.priority == ALARM_CLASS_PRIORITY::P_ALARM )
             {
